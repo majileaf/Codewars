01a93ec2b8ff36527e2f2b169650073b29331302
majileaf <99004780+majileaf@users.noreply.github.com>
2022-04-01 03:11:22 +0200

    Find nth root of number (2)

--- 6th Kyu Problems/Nth Root of a Number Redux.js
+++ 6th Kyu Problems/Nth Root of a Number Redux.js
@@ -1,6 +1,8 @@
 /*
 Task
-Given two numbers x and n, calculate the nth root of x, e.g. r such that r^n = x. However, you're working with some kind of legacy API that doesn't contain a helpful function for this task (see end of the description).
+Given two numbers x and n, calculate the nth root of x, e.g. r such that r^n = x. 
+However, you're working with some kind of legacy API that doesn't contain a helpful 
+function for this task (see end of the description).
 
 Examples
 root(4, 2);   // 2
@@ -9,7 +11,7 @@ root(256, 4); // 4
 root(9, 2);   // 3
 
 Disabled functions
-The power function of your language (e.g. Math.pow in JavaScript, ** in Haskell) has been disabled. Good luck.
+The power function of your language (e.g. Math.pow in JavaScript, ** in Haskell) has been disabled. 
 */
 
 const root = (x,n) => x ** (1/n)
