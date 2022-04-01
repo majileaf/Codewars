ddaa577c966c3d502e2cc9c73e077ec0226fa704
majileaf <99004780+majileaf@users.noreply.github.com>
2022-04-01 02:26:04 +0200

    Sum number array

--- Sum Arrays.js
+++ Sum Arrays.js
@@ -1,6 +1,7 @@
 /*
 Write a function that takes an array of numbers and returns the sum of the numbers. 
-The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+The numbers can be negative or non-integer. If the array does not contain any numbers
+then you should return 0.
 
 Examples
 Input: [1, 5.2, 4, 0, -1]
@@ -17,7 +18,8 @@ You can assume that you are only given numbers.
 You cannot assume the size of the array.
 You can assume that you do get an array and if the array is empty, return 0.
 What We're Testing
-We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
+We're testing basic loops and math operations. This is for beginners who are just learning 
+loops and math operations.
 Advanced users may find this extremely easy and can easily write this in one line.
 */
 
