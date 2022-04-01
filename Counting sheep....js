7adf82f37e8d4c947832ee4800d4827cbdab44c8
majileaf <99004780+majileaf@users.noreply.github.com>
2022-04-01 02:52:31 +0200

    Count boolean values in array

--- Counting sheep....js
+++ Counting sheep....js
@@ -1,8 +1,8 @@
 /*
-Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+Consider an array/list of sheep where some sheep may be missing from their place. 
+We need a function that counts the number of sheep present in the array (true means present).
 
-For example,
-
+For example:
 [true,  true,  true,  false,
   true,  true,  true,  true ,
   true,  false, true,  false,
