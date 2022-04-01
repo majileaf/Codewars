c47df78209994e8987892ea9116ff1d75eb98982
majileaf <99004780+majileaf@users.noreply.github.com>
2022-04-01 02:24:17 +0200

    Return sum of positive only array

--- Sum of positive.js
+++ Sum of positive.js
@@ -6,7 +6,7 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0.
 */
 
-let positiveSum = arr => {
+const positiveSum = arr => {
     let sum = i = 0;
     while (arr.length !== 0 && i < arr.length) {
         if (arr[i] > 0) sum += arr[i];
