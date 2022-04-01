11c9303be1e9a1e9ae098636d25e4fe43ab94fbc
majileaf <99004780+majileaf@users.noreply.github.com>
2022-04-01 02:50:13 +0200

    Even or odd number check

--- Even or Odd.js
+++ Even or Odd.js
@@ -1,5 +1,6 @@
 /*
-Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+Create a function that takes an integer as an argument and 
+returns "Even" for even numbers or "Odd" for odd numbers.
 */
 
 const even_or_odd = number => number % 2 === 0 ? "Even" : "Odd";
