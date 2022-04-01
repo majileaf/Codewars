f19838acf7998ec69909a52865fa098b9cd60ae5
majileaf <99004780+majileaf@users.noreply.github.com>
2022-04-01 02:58:52 +0200

    Convert boolean to string

--- Convert a Boolean to a String.js
+++ Convert a Boolean to a String.js
@@ -1,5 +1,6 @@
 /*
-Implement a function which convert the given boolean value into its string representation.
+Implement a function which convert the given 
+boolean value into its string representation.
 
 Note: Only valid inputs will be given.
 */
