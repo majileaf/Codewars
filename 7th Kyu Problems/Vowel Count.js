85eec0f7e8c1ec795fddab8b77231180f6a74880
Steven <majileaf@gmail.com>
2022-04-08 00:36:12 +0200

    Count vowels in string #2

--- 7th Kyu Problems/Vowel Count.js
+++ 7th Kyu Problems/Vowel Count.js
@@ -4,4 +4,11 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.
 */
 
-const getCount = str => str.split('').reduce((sum, n) => sum + ((n === 'a' || n === 'e' || n === 'i' || n === 'o' || n === 'u') ? 1 : 0), 0);
+// initial solution:
+// const getCount = str => str.split('').reduce((sum, n) => sum + ((n === 'a' || n === 'e' || n === 'i' || n === 'o' || n === 'u') ? 1 : 0), 0);
+
+// improved:
+const getCount = str => {
+    const vowels = ['a','e','i','o','u'];
+    return str.split('').reduce((sum, n) => sum + (vowels.includes(n) ? 1 : 0), 0);
+}
