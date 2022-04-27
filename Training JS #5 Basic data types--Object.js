c37ccbe02bffcfd06c2b85ba653564e97272cd53
Steven <majileaf@gmail.com>
2022-04-27 17:23:53 +0200

    Return object properties

---
+++ Training JS #5 Basic data types--Object.js
@@ -0,0 +1,27 @@
+/*
+In javascript, Object is one of basic data types. 
+Define an Object can use var obj=new Object() or var obj={}
+
+You can define the object attributes during initialization, like this:
+
+var animal={name:"dog"}
+you can also set/get some properties after the object definition, 
+like this:
+
+var animal={}
+animal.name="dog"  (or animal["name"]="dog")
+
+Task
+Give you a function animal, accept 1 parameter:obj like this:
+
+{name:"dog",legs:4,color:"white"}
+and return a string like this:
+
+"This white dog has 4 legs."
+*/
+
+const animal = obj => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+
+console.log(animal({name:"dog",legs:4,color:"white"})) // "This white dog has 4 legs."
+console.log(animal({name:"cock",legs:2,color:"red"})) // "This red cock has 2 legs."
+console.log(animal({name:"rabbit",legs:4,color:"gray"})) // "This gray rabbit has 4 legs."
