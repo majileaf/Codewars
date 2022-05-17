31624fe85fa322571944a7ac0a9e0cc7f2fb2468
Steven <majileaf@gmail.com>
2022-05-18 01:21:32 +0200

    Math condition check

---
+++ 7th Kyu Problems/Sum of a sequence.js
@@ -0,0 +1,33 @@
+/*
+Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+If begin value is greater than the end, function should returns 0
+
+Examples
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)
+*/
+
+// initial solution:
+// const sequenceSum = (begin, end, step) => {
+//     let sum = 0;
+//     for (let i = begin; i <= end; i += step) {
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// math solution:
+const sequenceSum = (begin, end, step) => {
+    if (begin > end) return 0;
+    const n = Math.floor((end - begin) / step + 1);
+    return n * (2 * begin + (n - 1) * step) / 2;
+}
+
+console.log(sequenceSum(2, 6, 2)) // 12
+console.log(sequenceSum(1, 5, 1)) // 15
+console.log(sequenceSum(1, 5, 3)) // 5
