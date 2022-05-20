ede13d4c0a887ac268ea1830c35bd2abdf34aa17
Steven <majileaf@gmail.com>
2022-05-20 22:20:58 +0200

    Conditional check

---
+++ 7th Kyu Problems/Fizz Buzz.js
@@ -0,0 +1,31 @@
+/*
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+N will never be less than 1.
+
+Method calling example:
+
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+*/
+
+// Return an array
+const fizzbuzz = n => {
+  let arr = [];
+  
+  for (let i = 1; i <= n; i++) {
+      switch(true){
+          case (i % 15 === 0): arr.push('FizzBuzz'); break;
+          case (i % 3 === 0): arr.push('Fizz'); break;
+          case (i % 5 === 0): arr.push('Buzz'); break;
+          default: arr.push(i);
+      }
+  }
+  return arr;
+}
+
+console.log(fizzbuzz(10)); // [1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz']
