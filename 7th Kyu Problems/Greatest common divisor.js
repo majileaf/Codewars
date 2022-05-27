753f0b29e364ecad49e8509628e457d3082707f4
Steven <majileaf@gmail.com>
2022-05-27 22:46:29 +0200

    Math condition check

---
+++ 7th Kyu Problems/Greatest common divisor.js
@@ -0,0 +1,25 @@
+/*
+Find the greatest common divisor of two positive integers. 
+The integers can be large, so you need to find a clever solution.
+
+The inputs x and y are always greater or equal to 1, so the greatest 
+common divisor will always be an integer that is also greater or equal to 1.
+*/
+
+const mygcd = (x,y) => {
+    if (x === 0) return y;
+    if (y === 0) return x;
+    let temp = 0;
+
+    do {
+        temp = x % y;
+        x = y;
+        y = temp;
+    } while (y !== 0)
+
+    return x;
+}
+
+console.log(mygcd(30,12)) // 6
+console.log(mygcd(8,9)) // 1
+console.log(mygcd(1,1)) // 1
