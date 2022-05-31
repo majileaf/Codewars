38691a4161d4827cb555c755e64b6bbe7907e0f5
Steven <majileaf@gmail.com>
2022-05-31 04:03:46 +0200

    Convert seconds to time format

---
+++ 5th Kyu Problems/Human Readable Time.js
@@ -0,0 +1,35 @@
+/*
+Write a function, which takes a non-negative integer (seconds) 
+as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+*/
+
+const humanReadable = seconds => {
+    let date = new Date(null);
+    date.setSeconds(seconds);
+
+    const time = date.toISOString();
+    const days = Number(time.slice(8, 10));
+    const hours = (days - 1) * 24 + Number(time.slice(11, 13));
+
+    return days > 1 
+        ? `${hours}` + time.slice(13, 19)
+        : time.slice(11, 19) ;
+}
+
+console.log(humanReadable(0)) // '00:00:00'
+console.log(humanReadable(59)) // '00:00:59'
+console.log(humanReadable(60)) // '00:01:00'
+console.log(humanReadable(90)) // '00:01:30'
+console.log(humanReadable(3599)) // '00:59:59'
+console.log(humanReadable(3600)) // '01:00:00'
+console.log(humanReadable(45296)) // '12:34:56'
+console.log(humanReadable(86399)) // '23:59:59'
+console.log(humanReadable(86400)) // '24:00:00'
+console.log(humanReadable(359999)) // '99:59:59'
