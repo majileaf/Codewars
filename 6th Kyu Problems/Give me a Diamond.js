701601897be5da50ee481065be8e374156811564
Steven <majileaf@gmail.com>
2022-06-05 04:34:13 +0200

    String manipulation

---
+++ 6th Kyu Problems/Give me a Diamond.js
@@ -0,0 +1,48 @@
+/* Give me a Diamond
+Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants 
+a diamond string from James. Since James doesn't know how to make this happen, 
+he needs your help.
+
+Task
+You need to return a string that looks like a diamond shape when printed on 
+the screen, using asterisk (*) characters. Trailing spaces should be removed, 
+and every line must be terminated with a newline character (\n).
+
+Return null/nil/None/... if the input is an even number or negative, as it is 
+not possible to print a diamond of even or negative size.
+
+Examples
+A size 3 diamond:
+
+ *
+***
+ *
+...which would appear as a string of " *\n***\n *\n"
+
+A size 5 diamond:
+
+  *
+ ***
+*****
+ ***
+  *
+...that is: "  *\n ***\n*****\n ***\n  *\n"
+*/
+
+const diamond = n => {
+  if (n < 0 || n % 2 === 0) return null;
+  let ham = [];
+  const burger = '*'.repeat(n) + '\n';
+
+  for (let i = 1; i < n; i += 2) {
+      ham.push(' '.repeat(Math.floor(n / 2) - Math.floor(i / 2)) + '*'.repeat(i) + '\n');
+  }
+  return ham.join('') + burger + ham.slice().reverse().join('');
+}
+
+console.log(diamond(1)) // "*\n"
+console.log(diamond(3)) // " *\n***\n *\n"
+console.log(diamond(5)) // "  *\n ***\n*****\n ***\n  *\n"
+console.log(diamond(2)) // null
+console.log(diamond(-3)) // null
+console.log(diamond(0)) // null
