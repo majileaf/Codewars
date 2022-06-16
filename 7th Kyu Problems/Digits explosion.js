11735670ca7a310051976b1a52ad4a62cf27ce9a
Steven <majileaf@gmail.com>
2022-06-16 17:20:31 +0200

    String manipulation

---
+++ 7th Kyu Problems/Digits explosion.js
@@ -0,0 +1,23 @@
+/* Digits explosion
+Given a string made of digits [0-9], return a string where 
+each digit is repeated a number of times equals to its value.
+
+Examples
+explode("312")
+
+should return :
+"333122"
+
+explode("102269")
+
+should return :
+"12222666666999999999"
+*/
+
+const explode = s => s.split('').map(e => e.repeat(e)).join('');
+
+console.log(explode("312")) // "333122"
+console.log(explode("102269")) // "12222666666999999999"
+console.log(explode("0")) // ""
+console.log(explode("000")) // ""
+console.log(explode("123")) // "122333"
