13b07c322685a9f117f8d23db322d07239104708
Steven <majileaf@gmail.com>
2022-06-23 18:23:09 +0200

    Math conditional check

---
+++ 7th Kyu Problems/Largest Square Inside A Circle.js
@@ -0,0 +1,10 @@
+/*
+Determine the area of the largest square that can fit inside a 
+circle with radius r.
+*/
+
+const areaLargestSquare = r => 2 * r ** 2;
+
+console.log(areaLargestSquare(5)) // 50
+console.log(areaLargestSquare(7)) // 98
+console.log(areaLargestSquare(15)) // 450
