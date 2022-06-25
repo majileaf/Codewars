2dbebceea4c4a240d43402a7af1d0280ce3c63d9
Steven <majileaf@gmail.com>
2022-06-25 16:57:36 +0200

    String manipulation

---
+++ 7th Kyu Problems/Switcheroo.js
@@ -0,0 +1,20 @@
+/*
+Given a string made up of letters a, b, and/or c, switch the position of 
+letters a and b (change a to b and vice versa). Leave any incidence of c 
+untouched.
+
+Example:
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+
+const switcheroo = x => [...x]
+    .map(e => 
+        e === 'a' ? 'b' : 
+        e === 'b' ? 'a' : 
+        e)
+    .join('');
+
+console.log(switcheroo('abc')) // 'bac'
+console.log(switcheroo('aaabcccbaaa')) // 'bbbacccabbb'
+console.log(switcheroo('ccccc')) // 'ccccc'
