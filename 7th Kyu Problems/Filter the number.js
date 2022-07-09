9f92f2c0ae1a919687a446c3777b47a5790b9fc5
Steven <majileaf@gmail.com>
2022-07-09 13:00:30 +0200

    String conditional check

---
+++ 7th Kyu Problems/Filter the number.js
@@ -0,0 +1,18 @@
+/*
+Oh, no! The number has been mixed up with the text. Your goal is to 
+retrieve the number from the text, can you return the number back to
+its original state?
+
+Task
+Your task is to return a number from a string.
+
+Details
+You will be given a string of numbers and letters mixed up, you have 
+to return all the numbers in that string in the order they occur.
+*/
+
+const FilterString = value => +[...value].filter(n => n >= '0' && n <= '9').join('');
+
+console.log(FilterString("123")) // 123
+console.log(FilterString("a1b2c3")) // 123
+console.log(FilterString("aa1bb2cc3dd")) // 123
