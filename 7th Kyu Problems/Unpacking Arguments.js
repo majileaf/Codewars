c17fa45f52ed61fcd1ef1bd4b719ad608a53d69a
Steven <majileaf@gmail.com>
2022-07-16 00:33:54 +0200

    Return result from function and its arguments

---
+++ 7th Kyu Problems/Unpacking Arguments.js
@@ -0,0 +1,16 @@
+/*
+You must create a function, spread, that takes a function and a list 
+of arguments to be applied to that function. You must make this function 
+return the result of calling the given function/lambda with the given
+arguments.
+
+eg:
+spread(someFunction, [1, true, "Foo", "bar"] ) 
+
+// is the same as...
+someFunction(1, true, "Foo", "bar")
+*/
+
+const spread = (func, args) => func(...args);
+
+console.log(spread(function(x,y){return x+y}, [1,2])) // 3 
