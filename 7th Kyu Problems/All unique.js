617fba22892208ac9fab1a35062f433007a4b6c4
Steven <majileaf@gmail.com>
2022-08-08 20:29:01 +0200

    String conditional check

---
+++ 7th Kyu Problems/All unique.js
@@ -0,0 +1,20 @@
+/* All unique
+Write a program to determine if a string contains only unique characters. 
+Return true if it does and false otherwise.
+
+The string may contain any of the 128 ASCII characters. Characters are 
+case-sensitive, e.g. 'a' and 'A' are considered different characters.
+*/
+
+// initial solution:
+// const hasUniqueChars = str => str === [...new Set(str)].join('');
+
+// alternatively:
+// const hasUniqueChars = str => str.length === [...new Set(str)].length
+
+const hasUniqueChars = str => str.length === new Set(str).size;
+
+console.log(hasUniqueChars("  nAa")) // false) // because there are two spaces ' '
+console.log(hasUniqueChars("abcdef")) // true)
+console.log(hasUniqueChars("aA")) // true) // case - sensitivity
+console.log(hasUniqueChars("++-")) // false) // because there are two '+'
