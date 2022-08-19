f706743aeb378111bf9ca2a0b4b15e72c579e337
Steven <majileaf@gmail.com>
2022-08-19 20:30:27 +0200

    String manipulation

---
+++ 7th Kyu Problems/Spacify.js
@@ -0,0 +1,11 @@
+/* Spacify
+Modify the spacify function so that it returns the given string with 
+spaces inserted between each character.
+
+spacify("hello world") // returns "h e l l o   w o r l d"
+*/
+
+const spacify = str => str.split('').join(' ');
+
+console.log(spacify('hello world')) // 'h e l l o   w o r l d'
+console.log(spacify('12345')) // '1 2 3 4 5'
