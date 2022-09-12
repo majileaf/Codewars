b00df1c3ddf4416359d22e9b7592d28e4a754e46
Steven <majileaf@gmail.com>
2022-09-12 21:11:07 +0200

    String conditional check

---
+++ 7th Kyu Problems/Cat and Mouse - Easy Version.js
@@ -0,0 +1,23 @@
+/* Cat and Mouse - Easy Version
+You will be given a string (x) featuring a cat 'C' and a mouse 'm'. 
+The rest of the string will be made up of '.'.
+
+You need to find out if the cat can catch the mouse from it's current position. 
+The cat can jump over three characters. So:
+
+C.....m returns 'Escaped!' <-- more than three characters between
+
+C...m returns 'Caught!' <-- as there are three characters between the two, 
+the cat can jump.
+*/
+
+const catMouse = x => {
+    let arr = x.split('');
+    return arr.indexOf('C') + 4 >= arr.indexOf('m') 
+        ? 'Caught!'
+        : 'Escaped!';
+}
+
+console.log(catMouse('C....m')) // "Escaped!"
+console.log(catMouse('C..m')) // "Caught!"
+console.log(catMouse('C.....m')) // "Escaped!"
