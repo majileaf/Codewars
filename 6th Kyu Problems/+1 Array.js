202d5f7835ce8d67bb88a9de38979ae230276a62
Steven <majileaf@gmail.com>
2022-10-20 23:27:43 +0200

    Array conditional check

---
+++ 6th Kyu Problems/+1 Array.js
@@ -0,0 +1,22 @@
+/* +1 Array
+Given an array of integers of any length, return an array that 
+has 1 added to the value represented by the array.
+
+the array can't be empty
+only non-negative, single digit integers are allowed
+Return nil (or your language's equivalent) for invalid inputs.
+
+Examples
+For example the array [2, 3, 9] equals 239, adding one would 
+return the array [2, 4, 0].
+
+[4, 3, 2, 5] would return [4, 3, 2, 6]
+*/
+
+function upArray(arr){
+  // ... 
+}
+
+console.log(upArray([2,3,9])) // [2,4,0]
+console.log(upArray([4,3,2,5])) // [4,3,2,6]
+console.log(upArray([1,-9])) // null
