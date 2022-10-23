dc3c8bb7d881fe6ab5fd04ac5c406901cbfa945e
Steven <majileaf@gmail.com>
2022-10-23 23:59:50 +0200

    String conditional check

---
+++ 7th Kyu Problems/Move 10.js
@@ -0,0 +1,15 @@
+/* Move 10
+Move every letter in the provided string forward 10 letters through the alphabet.
+
+If it goes past 'z', start again at 'a'.
+
+Input will be a string with length > 0.
+*/
+
+function moveTen(s){
+
+}
+
+console.log(moveTen("testcase")) // "docdmkco"
+console.log(moveTen("codewars")) // "mynogkbc"
+console.log(moveTen("exampletesthere")) // "ohkwzvodocdrobo"
