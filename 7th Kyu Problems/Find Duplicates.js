775ea62dfc028d3b302b8ff4c62311f222571ca5
Steven <majileaf@gmail.com>
2022-10-30 17:01:34 +0100

    Find duplicates in array

---
+++ 7th Kyu Problems/Find Duplicates.js
@@ -0,0 +1,17 @@
+/* Find Duplicates
+Given an array, find the duplicates in that array, and return a 
+new array of those duplicates. The elements of the returned array 
+should appear in the order when they first appeared as duplicates.
+
+Note: numbers and their corresponding string representations should 
+not be treated as duplicates (i.e., "1" != 1).
+
+Examples
+[1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]  ==>  [4, 3, 1]
+[0, 1, 2, 3, 4, 5]                ==>  []
+*/
+
+const duplicates2 = arr => [...new Set(arr.filter((e, i) => arr.indexOf(e) !== i))];
+
+console.log(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, '5'])) // [4, 3, 1]
+console.log(duplicates([0, 1, 2, 3, 4, 5])) // []
