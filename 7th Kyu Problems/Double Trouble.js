7eba0c8b04afa1b5ed71bbd514f2e20d688dbe55
Steven <majileaf@gmail.com>
2022-11-16 01:23:15 +0100

    Array conditional check - Filter number array in specific way - extra info

--- 7th Kyu Problems/Double Trouble.js
+++ 7th Kyu Problems/Double Trouble.js
@@ -13,6 +13,13 @@ Work through the array from left to right.
 Return the resulting array.
 */
 
+/* from a comment:
+It is unclear what "removing an element" means. As I thought first a case like a=[2, 6, 2], n=8 should 
+return [2] because the comparisons would be 2 + 6 == 8 and 6 + 2 == 8 (6 is removed but we still check 
+if a[1] + a[2] == n), while the correct comparisons are 2 + 6 == 8, 2 + 2 == 8 (6 is removed so we 
+check a[0] + a[2] == n instead), and the correct result is [2, 2].
+*/
+
 const trouble = (x, t) => x.reduce((sum, n, i) => (i !== x.length - 1 && t !== sum[sum.length - 1] + x[i + 1] ? sum.push(x[i + 1]) : sum, sum), [x[0]]);
 
 console.log(trouble([1, 3, 5, 6, 7, 4, 3],7)) // [1, 3, 5, 6, 7, 4]
