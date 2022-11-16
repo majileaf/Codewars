49e1e4a770c111bdf65c2280fdd5934d3b52760a
Steven <majileaf@gmail.com>
2022-11-16 01:21:52 +0100

    Array conditional check - Filter number array in specific way

--- 7th Kyu Problems/Double Trouble.js
+++ 7th Kyu Problems/Double Trouble.js
@@ -13,9 +13,7 @@ Work through the array from left to right.
 Return the resulting array.
 */
 
-function trouble(x, t){
-
-}
+const trouble = (x, t) => x.reduce((sum, n, i) => (i !== x.length - 1 && t !== sum[sum.length - 1] + x[i + 1] ? sum.push(x[i + 1]) : sum, sum), [x[0]]);
 
 console.log(trouble([1, 3, 5, 6, 7, 4, 3],7)) // [1, 3, 5, 6, 7, 4]
 console.log(trouble([4, 1, 1, 1, 4],2)) // [4, 1, 4]
