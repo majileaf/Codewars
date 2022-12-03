7004689a5c384a20b719b7b543844b039a04c964
Steven <majileaf@gmail.com>
2022-12-03 23:44:03 +0100

    String conditional check

---
+++ 7th Kyu Problems/The Office III - Broken Photocopier.js
@@ -0,0 +1,16 @@
+/* The Office III - Broken Photocopier
+The bloody photocopier is broken... Just as you were sneaking around the office 
+to print off your favourite binary code!
+
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+Given a string of binary, return the version the photocopier gives you as a string.
+*/
+
+function broken(x){
+
+}
+
+console.log(broken("1")) // "0"
+console.log(broken("10000000101101111110011001000")) // "01111111010010000001100110111"
+console.log(broken("100010")) // "011101"
