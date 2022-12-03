081c82506ead70f1fc3610d31477ed9deaa7eee4
Steven <majileaf@gmail.com>
2022-12-03 23:59:12 +0100

    String conditional check - replace 0 & 1

--- 7th Kyu Problems/The Office III - Broken Photocopier.js
+++ 7th Kyu Problems/The Office III - Broken Photocopier.js
@@ -7,9 +7,14 @@ Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
 Given a string of binary, return the version the photocopier gives you as a string.
 */
 
-function broken(x){
+// initial solution
+// const broken = x => [...x].map(e => Number(e) ? 0 : 1).join('');
 
-}
+// 1 - e
+// const broken = x => [...x].map(e => 1 - e).join('');
+
+// XOR
+const broken = x => [...x].map(e => e ^ 1).join('');
 
 console.log(broken("1")) // "0"
 console.log(broken("10000000101101111110011001000")) // "01111111010010000001100110111"
