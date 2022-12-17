fc66bd0d9ebbd8d2839f9af19232436723fbceb2
Steven <majileaf@gmail.com>
2022-12-17 01:09:12 +0100

    Math conditional check

--- 7th Kyu Problems/Collatz Conjecture Length.js
+++ 7th Kyu Problems/Collatz Conjecture Length.js
@@ -14,8 +14,13 @@ In the example above, the output would be 8.
 For more reading see: http://en.wikipedia.org/wiki/Collatz_conjecture
 */
 
-function collatz(n) {
-  //your code here
+const collatz = n => {
+  let arr = [n];
+  while (n !== 1) {
+      (n % 2 === 0) ? n = n / 2 : n = n * 3 + 1;
+      arr.push(n);
+  }
+  return arr.length;
 }
 
 console.log(collatz(20)) // 8
