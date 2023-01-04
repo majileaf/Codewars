87d75c6ac679d7e0de2a83bd6f80ec431abf1f83
Steven <majileaf@gmail.com>
2023-01-04 21:24:55 +0100

    String & Array conditional check

---
+++ 7th Kyu Problems/Filter Long Words.js
@@ -0,0 +1,12 @@
+/* Filter Long Words
+Write a function that takes a string and an an integer n as parameters and 
+returns a list of all words that are longer than n.
+
+Example:
+* With input "The quick brown fox jumps over the lazy dog", 4
+* Return ['quick', 'brown', 'jumps']
+*/
+
+const filterLongWords = (sentence, n) => sentence.split(' ').filter(word => word.length > n);
+
+console.log(filterLongWords("The quick brown fox jumps over the lazy dog", 4)) // ['quick', 'brown', 'jumps']
