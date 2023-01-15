c81a9de951b41a667e5265338fbc842a12ddd83a
Steven <majileaf@gmail.com>
2023-01-15 16:46:18 +0100

    Array conditional check

---
+++ 7th Kyu Problems/Find min and max.js
@@ -0,0 +1,10 @@
+/* Find min and max
+Implement a function that returns the minimal and the maximal value 
+of a list (in this order).
+*/
+
+const getMinMax = arr => [Math.min(...arr), Math.max(...arr)];
+
+console.log(getMinMax([1])) // [1,1]
+console.log(getMinMax([1,2])) // [1,2]
+console.log(getMinMax([2,1])) // [1,2]
