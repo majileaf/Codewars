14cc78150d9ba31a7b0d252100f50cd8678d02bd
Steven <majileaf@gmail.com>
2023-01-28 05:12:51 +0100

    String conditional check

---
+++ 7th Kyu Problems/Converting integer to currency format.js
@@ -0,0 +1,22 @@
+/* Converting integer to currency format
+Write a function that takes an integer in input and outputs a string with currency format.
+
+Integer in currency format is expressed by a string of number where every three characters 
+are separated by comma.
+
+For example:
+123456 --> "123,456"
+Input will always be an positive integer, so don't worry about type checking or 
+negative/float values.
+*/
+
+const toCurrency = price => [...String(price)]
+    .reverse()
+    .map((e, i) => i % 3 === 0 && i !== 0 ? e + ',' : e)
+    .reverse()
+    .join('');
+
+console.log(toCurrency(123456)) // "123,456"
+console.log(toCurrency(1234)) // "1,234"
+console.log(toCurrency(123)) // "123"
+console.log(toCurrency(123456789012)) // "123,456,789,012"
