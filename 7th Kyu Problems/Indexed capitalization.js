da9a6b5dd9766a14fc98a8cadb7cee731379ea26
Steven <majileaf@gmail.com>
2023-02-03 01:33:07 +0100

    String conditional check

--- 7th Kyu Problems/Indexed capitalization.js
+++ 7th Kyu Problems/Indexed capitalization.js
@@ -8,13 +8,25 @@ capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
 The input will be a lowercase string with no spaces and an array of digits.
 */
 
-function capitalize(s,arr){
-  return "";
-};
+// initial 
+// const capitalize = (s, arr) => {
+//     s = [...s];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < s.length) s[arr[i]] = s[arr[i]].toUpperCase();
+//     }
+//     return s.join('');
+// }
 
-console.log(capitalize("abcdef",[1,2,5]),'aBCdeF');
-console.log(capitalize("abcdef",[1,2,5,100]),'aBCdeF');
-console.log(capitalize("codewars",[1,3,5,50]),'cOdEwArs');
-console.log(capitalize("abracadabra",[2,6,9,10]),'abRacaDabRA');
-console.log(capitalize("codewarriors",[5]),'codewArriors');
-console.log(capitalize("indexinglessons",[0]),'Indexinglessons');
+const capitalize = (s, arr) => arr
+    .reduce((sum, n) => (n < sum.length 
+            ? sum[n] = sum[n].toUpperCase() 
+            : sum
+        , sum), [...s])
+    .join('');
+
+console.log(capitalize("abcdef",[1,2,5])) // 'aBCdeF'
+console.log(capitalize("abcdef",[1,2,5,100])) // 'aBCdeF'
+console.log(capitalize("codewars",[1,3,5,50])) // 'cOdEwArs'
+console.log(capitalize("abracadabra",[2,6,9,10])) // 'abRacaDabRA'
+console.log(capitalize("codewarriors",[5])) // 'codewArriors'
+console.log(capitalize("indexinglessons",[0])) // 'Indexinglessons'
