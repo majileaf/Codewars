e7fca8ca55abb02ecd9e62a53cb52ca226213e5c
Steven <majileaf@gmail.com>
2023-02-17 23:25:26 +0100

    Arguments conditional check

--- 7th Kyu Problems/Last.js
+++ 7th Kyu Problems/Last.js
@@ -10,9 +10,15 @@ In javascript and CoffeeScript a list will be an array,
 a string or the list of arguments.
 */
 
-const last2 = (...list) => (list.length === 1 && list[0].length > 0) 
-    ? list[0][list[0].length - 1] 
-    : list.pop();
+// initial solution:
+// const last2 = (...list) => (list.length === 1 && list[0].length > 0) 
+//     ? list[0][list[0].length - 1] 
+//     : list.pop();
+
+const last2 = (...list) => {
+    let last = list.pop();
+    return last[last.length - 1] || last;
+}
 
 console.log(last([1,2,3,4,5])) // 5   //-- array
 console.log(last("abcde")) // "e"     //-- string
