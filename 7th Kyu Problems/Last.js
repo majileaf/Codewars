fa6e9c291284b2c43a27d0b267918276ff529784
Steven <majileaf@gmail.com>
2023-02-17 20:50:33 +0100

    Arguments conditional check

---
+++ 7th Kyu Problems/Last.js
@@ -0,0 +1,19 @@
+/* Last
+Find the last element of the given argument(s).
+
+Examples
+last([1, 2, 3, 4] ) // =>  4
+last("xyz")         // => "z"
+last(1, 2, 3, 4)    // =>  4
+
+In javascript and CoffeeScript a list will be an array, 
+a string or the list of arguments.
+*/
+
+const last2 = (...list) => (list.length === 1 && list[0].length > 0) 
+    ? list[0][list[0].length - 1] 
+    : list.pop();
+
+console.log(last([1,2,3,4,5])) // 5   //-- array
+console.log(last("abcde")) // "e"     //-- string
+console.log(last(1,"b",3,"d",5)) // 5 //-- arguments
