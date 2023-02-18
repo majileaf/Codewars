a726fc3b91304251481083238ba5b2656ab4e4df
Steven <majileaf@gmail.com>
2023-02-18 22:58:56 +0100

    Math conditional check

---
+++ 7th Kyu Problems/Billiards triangle.js
@@ -0,0 +1,30 @@
+/* Billiards triangle
+Remember the triangle of balls in billiards? To build a 
+classic triangle (5 levels) you need 15 balls. With 3 balls 
+you can build a 2-level triangle, etc.
+
+For more examples,
+
+pyramid(1) == 1
+
+pyramid(3) == 2
+
+pyramid(6) == 3
+
+pyramid(10) == 4
+
+pyramid(15) == 5
+
+Write a function that takes number of balls (≥ 1) and calculates 
+how many levels you can build a triangle.
+*/
+
+function pyramid(balls) {
+  // your code here
+}
+
+console.log(pyramid(1)) // 1
+console.log(pyramid(4)) // 2
+console.log(pyramid(20)) // 5
+console.log(pyramid(100)) // 13
+console.log(pyramid(9999)) // 140
