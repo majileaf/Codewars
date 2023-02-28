4bbea5bc8ffb3d8a47cb20e363df29f09d247b1d
Steven <majileaf@gmail.com>
2023-02-28 23:43:16 +0100

    Array & String conditional check

---
+++ 7th Kyu Problems/Javascript filter - 1.js
@@ -0,0 +1,22 @@
+/* Javascript filter - 1
+While developing a website, you detect that some of the members have troubles logging in. 
+Searching through the code you find that all logins ending with a "_" make problems. 
+
+So you want to write a function that takes an array of pairs of login-names and e-mails, 
+and outputs an array of all login-name, e-mails-pairs from the login-names that end with "_".
+
+If you have the input-array:
+[ [ "foo", "foo@foo.com" ], [ "bar_", "bar@bar.com" ] ]
+
+it should output
+[ [ "bar_", "bar@bar.com" ] ]
+You have to use the filter-method which returns each element of the array for which the filter-method 
+returns true.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+*/
+
+const searchNames = logins => logins.filter(([loginName, email]) => loginName.endsWith('_'));
+
+const a2 = [ [ "foo", "foo@foo.com" ], [ "bar_", "bar@bar.com" ] ];
+console.log(searchNames(a2)) // [ [ "bar_", "bar@bar.com" ] ]
