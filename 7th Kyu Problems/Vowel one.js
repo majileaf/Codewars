4abaaf8de4c2b090df2436c2e5a62860102fdac4
Steven <majileaf@gmail.com>
2023-03-05 21:04:35 +0100

    String conditional check

---
+++ 7th Kyu Problems/Vowel one.js
@@ -0,0 +1,18 @@
+/* Vowel one
+Write a function that takes a string and outputs a strings of 1's and 0's 
+where vowels become 1's and non-vowels become 0's.
+
+All non-vowels including non alpha characters (spaces,commas etc.) 
+should be included.
+
+Examples:
+vowelOne( "abceios" ) // "1001110"
+vowelOne( "aeiou, abc" ) // "1111100100"
+*/
+
+function vowelOne(s){
+  // ...
+}
+
+console.log(vowelOne("vowelOne")) // "01010101" 
+console.log(vowelOne("123, arou")) // "000001011" 
