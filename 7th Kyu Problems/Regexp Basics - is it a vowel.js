1bdf300513c65852470c6f23730951a4a8a86015
Steven <majileaf@gmail.com>
2023-03-29 21:20:17 +0200

    String conditional check (regex)

---
+++ 7th Kyu Problems/Regexp Basics - is it a vowel.js
@@ -0,0 +1,16 @@
+/* Regexp Basics - is it a vowel?
+Implement the function which should return true if given object is 
+a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false 
+otherwise.
+*/
+
+String.prototype.vowel = function() {
+  return (/^[aeiou]$/i).test(this);
+};
+
+console.log(''.vowel()) // false
+console.log('a'.vowel()) // true
+console.log('E'.vowel()) // true
+console.log('ou'.vowel()) // false
+console.log('z'.vowel()) // false
+console.log('lol'.vowel()) // false
