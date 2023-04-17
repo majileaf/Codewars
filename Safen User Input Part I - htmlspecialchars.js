644c1fd933d2a8b1ad6c7b8db897004386f35126
Steven <majileaf@gmail.com>
2023-04-17 20:53:25 +0200

    String conditional check (regex)

--- Safen User Input Part I - htmlspecialchars.js
+++ Safen User Input Part I - htmlspecialchars.js
@@ -26,6 +26,17 @@ const htmlspecialchars = formData => formData
         '&': '&amp;'
     }[char]));
 
+// alternatively:
+// function htmlspecialchars(formData) {
+//   const dict = {
+//       '<': '&lt;',
+//       '>': '&gt;',
+//       '"': '&quot;',
+//       '&': '&amp;',
+//   };
+//   return formData.replace(/[<>"&]/g, char => dict[char]);
+// }
+
 console.log(htmlspecialchars("<h2>Hello World</h2>")) // "&lt;h2&gt;Hello World&lt;/h2&gt;"
 console.log(htmlspecialchars("Hello, how would you & I fare?")) // "Hello, how would you &amp; I fare?"
 console.log(htmlspecialchars('How was "The Matrix"?  Did you like it?')) // 'How was &quot;The Matrix&quot;?  Did you like it?'
