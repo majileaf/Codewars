be21494d30077aa9a46f66a322e1ba1380e19916
Steven <majileaf@gmail.com>
2023-04-25 22:56:34 +0200

    String conditional check

---
+++ 7th Kyu Problems/Merge overlapping strings.js
@@ -0,0 +1,26 @@
+/* Merge overlapping strings
+This kata requires you to write a function which merges two strings together. 
+It does so by merging the end of the first string with the start of the second 
+string together when they are an exact match.
+
+"abcde" + "cdefgh" => "abcdefgh"
+"abaab" + "aabab" => "abaabab"
+"abc" + "def" => "abcdef"
+"abc" + "abc" => "abc"
+NOTE: The algorithm should always use the longest possible overlap.
+
+"abaabaab" + "aabaabab" would be "abaabaabab" and not "abaabaabaabab"
+*/
+
+const mergeStrings = (first, second) => {
+  for (let i = 0; i < first.length; i++) {
+      if (second.includes(first.slice(i)) && second.split(first.slice(i))[0] === '') return first.slice(0, i) + second
+  }
+  return first + second;
+}
+
+console.log(mergeStrings('abcde','cdefgh')) // 'abcdefgh'
+console.log(mergeStrings('abaab','aabab')) // 'abaabab'
+console.log(mergeStrings('abc','def')) // 'abcdef'
+console.log(mergeStrings('abc','abc')) // 'abc'
+console.log(mergeStrings('abaabaab','aabaabab')) // 'abaabaabab'
