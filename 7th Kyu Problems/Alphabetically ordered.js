7d1be92cbc09c5ca80d769b9c15bf25ca6598b78
Steven <majileaf@gmail.com>
2023-06-06 23:54:45 +0200

    String conditional check

---
+++ 7th Kyu Problems/Alphabetically ordered.js
@@ -0,0 +1,21 @@
+/* Alphabetically ordered
+Your task is very simple. Just write a function that takes an input string of lowercase 
+letters and returns true/false depending on whether the string is in alphabetical order 
+or not.
+
+Examples (input -> output)
+"kata" -> false ('a' comes after 'k')
+"ant" -> true (all characters are in alphabetical order)
+*/
+
+function alphabetic(s) {
+  // Yor code here
+  return false;
+}
+
+console.log(alphabetic("asd")) // false
+console.log(alphabetic("codewars")) // false
+console.log(alphabetic("door")) // true
+console.log(alphabetic("cell")) // true
+console.log(alphabetic("z")) // true
+console.log(alphabetic("")) // true); 
