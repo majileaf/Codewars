e3e5910a12db61df7e725492cd1d79ed093e8cb9
Steven <majileaf@gmail.com>
2023-06-09 23:26:27 +0200

    Math conditional check - alternatively

--- 7th Kyu Problems/Sum - Square Even, Root Odd.js
+++ 7th Kyu Problems/Sum - Square Even, Root Odd.js
@@ -8,5 +8,8 @@ The list will never be empty and will only contain values that are greater than 
 
 const sumSquareEvenRootOdd = ns => +ns.reduce((sum, n) => sum + (n % 2 ? Math.sqrt(n) : n * n), 0).toFixed(2);
 
+// alternatively
+// const sumSquareEvenRootOdd = ns => +ns.reduce((sum, n) => sum + (n % 2 ? n ** 0.5 : n ** 2), 0).toFixed(2);
+
 console.log(sumSquareEvenRootOdd([4,5,7,8,1,2,3,0])) // 91.61
 console.log(sumSquareEvenRootOdd([1,14,9,8,17,21])) // 272.71
