5f344d3dd9e0c9569da6240ce62aaa677c80f6d2
Steven <majileaf@gmail.com>
2023-06-15 23:41:33 +0200

    String conditional check

---
+++ 7th Kyu Problems/Hëävÿ Mëtäl Ümläüts.js
@@ -0,0 +1,29 @@
+/* Hëävÿ Mëtäl Ümläüts
+Your retro heavy metal band, VÄxën, originally started as kind of a joke, just because why would anyone want to 
+use the crappy foosball table in your startup's game room when they could be rocking out at top volume in there 
+instead? Yes, a joke, but now all the top tech companies are paying you top dollar to play at their conferences 
+and big product-release events. And just as how in the early days of the Internet companies were naming everything
+
+Well, with great power chords there must also come great responsibility! You need to help these companies out by 
+writing a function that will guarantee that their web sites and ads and everything else will 
+RÖCK ÄS MÜCH ÄS PÖSSÏBLË! Just think about it -- with the licensing fees you'll be getting from Gööglë, Fäcëböök, 
+Äpplë, and Ämäzön alone, you'll probably be able to end world hunger, make college and Marshall stacks free to all, 
+and invent self-driving bumper cars. Sö lët's gët röckïng and röllïng! Pëdal tö thë MËTÄL!
+
+Here's a little cheatsheet that will help you write your function to replace the so-last-year letters 
+a-e-i-o-u-and-sometimes-y with the following totally rocking letters:
+
+A = Ä = \u00c4     E = Ë = \u00cb     I = Ï = \u00cf
+O = Ö = \u00d6     U = Ü = \u00dc     Y = Ÿ = \u0178
+a = ä = \u00e4     e = ë = \u00eb     i = ï = \u00ef
+o = ö = \u00f6     u = ü = \u00fc     y = ÿ = \u00ff
+*/
+
+function heavyMetalUmlauts(boringText) {
+    return "";
+}
+
+console.log(heavyMetalUmlauts("Announcing the Macbook Air Guitar")) // "Ännöüncïng thë Mäcböök Äïr Güïtär"
+console.log(heavyMetalUmlauts("Facebook introduces new heavy metal reaction buttons")) // "Fäcëböök ïntrödücës nëw hëävÿ mëtäl rëäctïön büttöns"
+console.log(heavyMetalUmlauts("Strong sales of Google's VR Metalheadsets send tech stock prices soaring")) // "Ströng sälës öf Gööglë's VR Mëtälhëädsëts sënd tëch stöck prïcës söärïng"
+console.log(heavyMetalUmlauts("Vegan Black Metal Chef hits the big time on Amazon TV")) // "Vëgän Bläck Mëtäl Chëf hïts thë bïg tïmë ön Ämäzön TV"
