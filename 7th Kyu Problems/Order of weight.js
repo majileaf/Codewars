6587dbab07d69fb8250e7c8ab8332e817a192de7
Steven <majileaf@gmail.com>
2023-07-17 03:50:42 +0200

    Array conditional check

--- 7th Kyu Problems/Order of weight.js
+++ 7th Kyu Problems/Order of weight.js
@@ -6,9 +6,26 @@ Weight units are grams(G), kilo-grams(KG) and tonnes(T).
 Arrays will always contain correct and positive values aswell as uppercase letters.
 */
 
-function arrange(arr){
+// intial solution:
+// const arrange = arr => {
+//     const unit = arr.reduce((sum, n) => (sum[n] = convert(n), sum), {});
+//     return Object.keys(unit).sort((a, b) => unit[a] - unit[b]);
+// }
+// const convert = str => 
+//     str.includes('T')  ? +str.split('T')[0] * 10e6 :
+//     str.includes('KG') ? +str.split('KG')[0] * 10e3  
+//     : +str.split('G')[0];
 
-}
+// improving #1
+// const arrange = arr => arr.sort((a, b) => convert(a) - convert(b));
+// const convert = str => 
+//     str.includes('T')  ? +str.split('T')[0] * 10e6 :
+//     str.includes('KG') ? +str.split('KG')[0] * 10e3  
+//     : +str.split('G')[0];
+
+// improving #2
+const arrange = arr => arr.sort((a, b) => convert(a) - convert(b));
+const convert = str => +str.replace(/KG|T|G/g, char => ({'T': '000000', 'KG': '000', 'G': ''})[char]);
 
 console.log(arrange(["200G","300G","150G","100KG"])) // ["150G","200G","300G","100KG"]
 console.log(arrange(["400G","100T","150KG","100G"])) // ["100G","400G","150KG","100T"]
