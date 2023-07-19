8a8accec76b6fce6b9db73c44c919c2a4eb21da2
Steven <majileaf@gmail.com>
2023-07-19 21:42:19 +0200

    String manipulation

---
+++ 7th Kyu Problems/Alphabetical Sequence.js
@@ -0,0 +1,22 @@
+/* Alphabetical Sequence
+In this kata you will be given a random string of letters and tasked with returning them as a string of comma-separated sequences 
+sorted alphabetically, with each sequence starting with an uppercase character followed by n-1 lowercase characters, where n is
+the letter's alphabet position 1-26.
+
+Example
+alphaSeq("ZpglnRxqenU") -> "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"
+
+Technical Details
+The string will include only letters.
+The first letter of each sequence is uppercase followed by n-1 lowercase.
+Each sequence is separated with a comma.
+Return value needs to be a string.
+*/
+
+const alphaSeq = str => [...str.toLowerCase()].sort().map(e => e.toUpperCase() + e.repeat(e.charCodeAt() - 97)).join(',');
+
+console.log(alphaSeq("ZpglnRxqenU")) // "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"
+console.log(alphaSeq("NyffsGeyylB")) // "Bb,Eeeee,Ffffff,Ffffff,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Sssssssssssssssssss,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy"
+console.log(alphaSeq("MjtkuBovqrU")) // "Bb,Jjjjjjjjjj,Kkkkkkkkkkk,Mmmmmmmmmmmmm,Ooooooooooooooo,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Tttttttttttttttttttt,Uuuuuuuuuuuuuuuuuuuuu,Uuuuuuuuuuuuuuuuuuuuu,Vvvvvvvvvvvvvvvvvvvvvv"
+console.log(alphaSeq("EvidjUnokmM")) // "Dddd,Eeeee,Iiiiiiiii,Jjjjjjjjjj,Kkkkkkkkkkk,Mmmmmmmmmmmmm,Mmmmmmmmmmmmm,Nnnnnnnnnnnnnn,Ooooooooooooooo,Uuuuuuuuuuuuuuuuuuuuu,Vvvvvvvvvvvvvvvvvvvvvv"
+console.log(alphaSeq("HbideVbxncC")) // "Bb,Bb,Ccc,Ccc,Dddd,Eeeee,Hhhhhhhh,Iiiiiiiii,Nnnnnnnnnnnnnn,Vvvvvvvvvvvvvvvvvvvvvv,Xxxxxxxxxxxxxxxxxxxxxxxx"
