fdd89984f3e437dca86cbda0d09bc5c0ae5f2fba
Steven <majileaf@gmail.com>
2023-08-02 20:57:16 +0200

    String & Array conditional check

--- 7th Kyu Problems/Sort the climbing grades.js
+++ 7th Kyu Problems/Sort the climbing grades.js
@@ -10,9 +10,24 @@ If the input is an empty list, return an empty list; otherwise the input will al
 valid list of one or more grades.
 */
 
-function sortGrades(lst){
-  //your code here
+// initial solution:
+// const sortGrades = lst => {
+//     const grades = ['VB', 'V0', 'V0+', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17']
+//     return lst.slice().sort((a, b) => grades.indexOf(a) - grades.indexOf(b));
+// }
+
+const sortGrades = lst => {
+    const grades = ['VB', 'V0', 'V0+'];
+    return lst.slice().sort((a, b) => getNum(a) - getNum(b) || grades.indexOf(a) - grades.indexOf(b));
 }
+
+const getNum = str => Number(str.split('').filter(e => e <= 9 && e >= 0).join(''));
+
+// alternatively: #1
+// const sortGrades = grades => {
+//   const value = x => ({ VB: -1, 'V0+': 0.5 }[x] || Number(x.slice(1)));
+//   return grades.sort((a, b) => value(a) - value(b));
+// }
 
 console.log(sortGrades(["V7", "V12", "V1"])) // ["V1", "V7", "V12"]
 console.log(sortGrades(["V13", "V14", "VB", "V0"])) // ["VB", "V0", "V13", "V14"]
