9060aaba4ee6e9d47c80230a038365dd856a9773
Steven <majileaf@gmail.com>
2023-08-09 23:54:41 +0200

    String & Array conditional check

--- 7th Kyu Problems/Alphabetize a list by the nth character.js
+++ 7th Kyu Problems/Alphabetize a list by the nth character.js
@@ -12,9 +12,17 @@ The length of all words provided in the list will be >= n. The format will be "x
 list of Strings instead.
 */
 
-function sortIt(list, n) {
-  //...
-}
+const sortIt = (list, n) => list.split(', ').sort((a, b) => a[n - 1].toLowerCase().localeCompare(b[n - 1].toLowerCase()) || a.localeCompare(b)).join(', ');
+
+// alternatively: 
+// const sortIt = (list, n) => list
+//     .split(', ')
+//     .sort((a, b) => {
+//         const A = a.toLowerCase().charCodeAt(n - 1);
+//         const B = b.toLowerCase().charCodeAt(n - 1);
+//         return A !== B ? A - B : a.localeCompare(b);
+//     })
+//     .join(', ');
 
 console.log(sortIt('bill, bell, ball, bull', 2)) // 'ball, bell, bill, bull'
 console.log(sortIt('cat, dog, eel, bee', 3)) // 'bee, dog, eel, cat'
