ff71c60428c678d8f4559181f852a931874a1241
Steven <majileaf@gmail.com>
2023-08-14 22:19:25 +0200

    String conditional check

--- 7th Kyu Problems/Olympic Rings.js
+++ 7th Kyu Problems/Olympic Rings.js
@@ -15,7 +15,21 @@ if score is 1 or less, return 'Not even a medal!'; if score is 2, return 'Bronze
 Dots over i's and any other letters don't count as rings.
 */
 
-function olympicRing(a){
+const olympicRing = a => {
+    const rings = {
+        'a': 1, 'b': 1, 'd': 1, 
+        'e': 1, 'g': 1, 'o': 1, 
+        'p': 1, 'q': 1, 'A': 1, 
+        'B': 2, 'D': 1, 'O': 1, 
+        'P': 1, 'Q': 1, 'R': 1, 
+    }
+
+    const score = Math.floor([...a].reduce((sum, n) => sum + (rings[n] || 0), 0) / 2);
+    
+    return score < 2 ? 'Not even a medal!' 
+         : score === 2 ? 'Bronze!' 
+         : score === 3 ? 'Silver!' 
+         : 'Gold!';
 }
 
 console.log(olympicRing('wHjMudLwtoPGocnJ')) // 'Bronze!'
