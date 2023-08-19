269f1fbd01ee4cb646da294122a8c6d16021bbf6
Steven <majileaf@gmail.com>
2023-08-19 15:48:54 +0200

    String manipulation (improved)

--- 5th Kyu Problems/Translate DNA in 6 frames.js
+++ 5th Kyu Problems/Translate DNA in 6 frames.js
@@ -68,9 +68,15 @@ The function should return an array with all translation asked for, by default t
 on all 6 frames.
 */
 
-function translateWithFrame(dna, frames=[1,2,3,-1,-2,-3]){
-  //your code here - use the preloaded object codons for conversions
-}
+var codons = {TTC: 'F', TTT: 'F', TTA: 'L', TTG: 'L', CTT: 'L', CTC: 'L', CTA: 'L', CTG: 'L', ATT: 'I', ATC: 'I', ATA: 'I', ATG: 'M', GTT: 'V', GTC: 'V', GTA: 'V', GTG: 'V', TCT: 'S', TCC: 'S', TCA: 'S', TCG: 'S', AGT: 'S', AGC: 'S', CCT: 'P', CCC: 'P', CCA: 'P', CCG: 'P', ACT: 'T', ACC: 'T', ACA: 'T', ACG: 'T', GCT: 'A', GCC: 'A', GCA: 'A', GCG: 'A', TAT: 'Y', TAC: 'Y', CAT: 'H', CAC: 'H', CAA: 'Q', CAG: 'Q', AAT: 'N', AAC: 'N', AAA: 'K', AAG: 'K', GAT: 'D', GAC: 'D', GAA: 'E', GAG: 'E', TGT: 'C', TGC: 'C', TGG: 'W', CGT: 'R', CGC: 'R', CGA: 'R', CGG: 'R', AGA: 'R', AGG: 'R', GGT: 'G', GGC: 'G', GGA: 'G', GGG: 'G', TAA: '*', TGA: '*', TAG: '*'};
+
+const translateWithFrame = (dna, frames = [1, 2, 3, -1, -2, -3]) => frames.map(e => translateDNA(tripletsARR(getStrand(dna, e), e)));
+const tripletsARR = (str, frame) => str.slice(Math.abs(frame) - 1).match(/[ATGC]{3}/g) || [];
+const getStrand = (dna, frame) => frame > 0 ? dna : reverseComplement(dna);
+const reverseComplement = str => [...str.replace(/[AGTC]/g, char => ({'A': 'T', 'G': 'C', 'T': 'A', 'C': 'G'})[char])].reverse().join('');
+const translateDNA = arr => arr.map(e => codons[e]).join('');
+
+console.log(translateWithFrame('AGGTGACACCGCAAGCCTTATATTAGC')) // ['R*HRKPYIS', 'GDTASLIL', 'VTPQALY*', 'PLWRSEY', 'STVAFGI', 'IHCGVRN']
 
 console.log(translateWithFrame("AAA",[1])) // ["K"]
 console.log(translateWithFrame("AAA",[2])) // [""]
@@ -81,5 +87,5 @@ console.log(translateWithFrame("AGGTGACACCGCAAGCCTTATATTAGC")) //["R*HRKPYIS","G
 console.log(translateWithFrame("AGGTGACACCGCAAGCCTTATATTAGC",[1,2])) // ["R*HRKPYIS","GDTASLIL"]
 console.log(translateWithFrame("AGGTGACACCGCAAGCCTTATATTAGC",[2,1])) // ["GDTASLIL","R*HRKPYIS"]
 console.log(translateWithFrame("AGGTGACACCGCAAGCCTTATATTAGC",[])) // []
-console.log(translateWithFrame("")) // ["", "", "", "", "", ""
+console.log(translateWithFrame("")) // ["", "", "", "", "", ""]
 console.log(translateWithFrame("",[])) // []
