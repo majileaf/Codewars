ec27dac49fa2b0fed631e0b09bbb26c34a67c8da
Steven <majileaf@gmail.com>
2023-08-22 23:34:19 +0200

    String manipulation

---
+++ 7th Kyu Problems/Make a square box!.js
@@ -0,0 +1,31 @@
+/* Make a square box!
+Given a number as a parameter (between 2 and 30), return an array containing strings which form a box.
+Like this:
+
+n = 5
+
+[
+  '-----',
+  '-   -',
+  '-   -',
+  '-   -',
+  '-----'
+]
+n = 3
+
+[
+  '---',
+  '- -',
+  '---'
+]
+*/
+
+function box(){
+  //your code here
+}
+
+console.log(box(5)) // ["-----", "-   -", "-   -", "-   -", "-----"]
+console.log(box(2)) // ["--", "--"]
+console.log(box(4)) // ["----", "-  -", "-  -", "----"]
+console.log(box(6)) // ["------", "-    -", "-    -", "-    -", "-    -", "------"]
+console.log(box(3)) // ["---", "- -", "---"]
