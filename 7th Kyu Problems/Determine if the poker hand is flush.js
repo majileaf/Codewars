9aad4a9f77ff712d24c86f6b367831a2593be72d
Steven <majileaf@gmail.com>
2023-09-05 14:11:22 +0200

    String & Array conditional check

--- 7th Kyu Problems/Determine if the poker hand is flush.js
+++ 7th Kyu Problems/Determine if the poker hand is flush.js
@@ -3,7 +3,7 @@ Determine if the poker hand is flush, meaning if the five cards are of the same 
 
 Your function will be passed a list/array of 5 strings, each representing a poker card 
 in the format "5H" (5 of hearts), meaning the value of the card followed by the initial
- of its suit (Hearts, Spades, Diamonds or Clubs). No jokers included.
+of its suit (Hearts, Spades, Diamonds or Clubs). No jokers included.
 
 Your function should return true if the hand is a flush, false otherwise.
 
@@ -14,9 +14,7 @@ Examples
 ["AD", "4S", "7H", "KS", "10S"] ==> false
 */
 
-function isFlush(cards) {
-  return true || false ;
-}
+const isFlush = cards => cards.every(e => e.slice(-1) === cards[0].slice(-1));
 
 console.log(isFlush([  "AS", "3S",  "9S", "KS", "4S" ])) //  true 
 console.log(isFlush([  "AD", "4S",  "7H", "KC", "5S" ])) // false 
