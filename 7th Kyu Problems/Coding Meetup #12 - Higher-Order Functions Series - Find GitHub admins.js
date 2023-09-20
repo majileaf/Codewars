1a05ce5a88aab24332b79bb71e8317250833ef29
Steven <majileaf@gmail.com>
2023-09-20 16:55:46 +0200

    Object conditional check

--- 7th Kyu Problems/Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins.js
+++ 7th Kyu Problems/Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins.js
@@ -25,20 +25,13 @@ The strings representing a given language will always be formatted in the same w
 (e.g. 'JavaScript' will always be formatted with upper-case 'J' and 'S'.
 */
 
-function findAdmin(list, lang) {
-  // thank you for checking out the Coding Meetup kata :)
-}
+const findAdmin = (list, lang) => list.filter(e => e.language === lang && e.githubAdmin === 'yes');
 
 var list1 = [
   { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
   { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
   { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
-];
-
-var answer1 = [
-  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
-  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' }
 ];
 
 console.log(findAdmin(list1, 'JavaScript')) 
