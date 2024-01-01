390d2776ed27d395447fb979ac91fddecdadb88b
Steven <majileaf@gmail.com>
2024-01-01 04:09:31 +0100

    String manipulation

---
+++ 7th Kyu Problems/Do you speak retsec.js
@@ -0,0 +1,31 @@
+/* Do you speak retsec?
+Do you speak retsec?
+You and your friends want to play undercover agents. In order to exchange your secret messages, 
+you've come up with the following system: you take the word, cut it in half, and place the first 
+half behind the latter. 
+
+If the word has an uneven number of characters, you leave the middle at its previous place:
+retsec examples
+That way, you'll be able to exchange your messages in private.
+
+Task
+You're given a single word. Your task is to swap the halves. If the word has an uneven length, 
+leave the character in the middle at that position and swap the chunks around it.
+
+Examples
+reverseByCenter("secret")  == "retsec" // no center character
+reverseByCenter("agent")   == "nteag"  // center character is "e"
+
+Remarks
+Don't use this to actually exchange messages in private.
+*/
+
+const reverseByCenter = s => {
+  const x = s.length / 2;
+  return s.slice(Math.ceil(x)) + s.slice(x, Math.ceil(x)) + s.slice(0, x);
+}
+
+console.log(reverseByCenter("secret")) // "retsec"
+console.log(reverseByCenter("agent")) // "nteag"
+console.log(reverseByCenter("raw")) // "war"
+console.log(reverseByCenter("onion")) // "onion"
