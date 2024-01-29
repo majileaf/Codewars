2b2bc89100840338ca5b4ba421118cf466600a6d
Steven <majileaf@gmail.com>
2024-01-29 19:05:16 +0100

    Object conditional check + Recursion

--- 7th Kyu Problems/Count strings in objects.js
+++ 7th Kyu Problems/Count strings in objects.js
@@ -12,9 +12,29 @@ strCount({
   //returns 3
 */
 
-function strCount(obj){
-// Your code here
-}
+// const strCount = (obj, i = 0) => typeof obj === 'string' 
+//     ? i = i + 1 
+//     : i + (Object.prototype.toString.call(obj) == '[object Object]' || Array.isArray(obj) 
+//         ? Object.values(obj).reduce((sum, n) => sum + (typeof n === 'string' ? 1 : strCount(n)), 0) 
+//         : 0);
+
+// alternatively:
+const strCount = obj => typeof obj === 'string' 
+    ? 1
+    : Object.values(obj || {}).reduce((sum, n) => sum + strCount(n), 0);
+
+console.log(strCount(null)); // 0
+console.log(strCount(undefined)); // 0
+console.log(strCount('asd')); // 1
+console.log(strCount({
+    first:  "1",
+    second: "2",
+    third:  false,
+    fourth: ["anytime",2,3,4],
+    // fifth:  null,
+    // sixth:  undefined,
+    seventh:{}
+  })); // 3
 
 console.log(strCount({
     first:  "1",
@@ -24,4 +44,7 @@ console.log(strCount({
     fifth:  null,
     sixth:  undefined,
     seventh:{}
-  })) // 3
+  })); // 3
+
+console.log(strCount(["anytime", 'yes',3,4])) // 2
+console.log(strCount(["anytime", 'yes', ['asd', 'dsa', ['asd']], 4])) // 5
