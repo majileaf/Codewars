c617a151b5811f141ea8932f3c586764565c1a27
Steven <majileaf@gmail.com>
2024-02-19 23:42:59 +0100

    Math & Algorithm conditional check

---
+++ 7th Kyu Problems/The alphabet product.js
@@ -0,0 +1,16 @@
+/* The alphabet product
+I have four positive integers, A, B, C and D, where A < B < C < D. 
+The input is a list of the integers A, B, C, D, AxB, BxC, CxD, DxA in some order. 
+Your task is to return the value of D.
+*/
+
+function alphabet(ns) {
+  return 0;
+}
+
+console.log(alphabet([2,3,4,1,12,6,2,4])) // 4 
+console.log(alphabet([2,6,7,3,14,35,15,5])) // 7 
+console.log(alphabet([20,10,6,5,4,3,2,12])) // 5 
+console.log(alphabet([2,6,18,3,6,7,42,14])) // 7 
+console.log(alphabet([7,96,8,240,12,140,20,56])) // 20 
+console.log(alphabet([20,30,6,7,4,42,28,5])) // 7 
