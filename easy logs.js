aa8e6f971d0d06beebb060f5da2fec4c48921b4f
Steven <majileaf@gmail.com>
2024-02-25 01:56:56 +0100

    Math conditional check

---
+++ easy logs.js
@@ -0,0 +1,12 @@
+/* easy logs
+Given a logarithm base X and two values A and B, return a sum of logratihms with the base X: 
+logX​A+logX​B
+*/
+
+function logs(x , a, b){
+//your code here
+}
+
+console.log(logs(10, 2, 3)) //   0.7781512503836435
+console.log(logs(5, 2, 3)) //    1.1132827525593785
+console.log(logs(1000, 2, 3)) // 0.25938375012788123
