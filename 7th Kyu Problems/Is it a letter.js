a0efb9cd473bf748574e6677c24f220b7c2b7673
Steven <majileaf@gmail.com>
2024-03-25 16:28:45 +0100

    String conditional check

---
+++ 7th Kyu Problems/Is it a letter.js
@@ -0,0 +1,11 @@
+/* Is it a letter?
+Write a function, isItLetter or is_it_letter, which tells us if a given character is an, 
+uppercase or lowercase, letter.
+*/
+
+function isItLetter(character) {
+  return true || false ;
+}
+
+console.log(isItLetter('a')) // true
+console.log(isItLetter('1')) // false
