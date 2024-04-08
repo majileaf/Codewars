f8a896793a2dff837adaf640db4dcaa754aa528f
Steven <majileaf@gmail.com>
2024-04-08 03:20:58 +0200

    Math conditional check

---
+++ 7th Kyu Problems/Cogs 2.js
@@ -0,0 +1,37 @@
+/* Cogs 2
+Kata Task
+You are given a list of cogs in a gear train
+
+Each element represents the number of teeth of that cog
+
+e.g. [100, 50, 25] means
+
+1st cog has 100 teeth
+2nd cog has 50 teeth
+3rd cog has 25 teeth
+If the nth cog rotates clockwise at 1 RPM what is the RPM of the cogs at each end of the gear train?
+
+Notes
+
+no two cogs share the same shaft
+return an array whose two elements are RPM of the first and last cogs respectively
+use negative numbers for anti-clockwise rotation
+for convenience n is zero-based
+For C and NASM coders, the returned array will be free'd.
+*/
+
+const cogRpm = (cogs, n) => {
+  const rRpm = cogs[n] / cogs[cogs.length - 1];
+  const lRpm = cogs[n] / cogs[0];
+  const sign = arr => (x, y) => arr.slice(x, y + 1).length % 2 ? 1 : -1;
+  return [sign(cogs)(0, n) * lRpm, sign(cogs)(n, cogs.length) * rRpm];
+}
+
+console.log(cogRpm([100, 50, 25], 1)) // [-0.5, -2]
+console.log(cogRpm([100, 100, 50, 25], 2)) // [0.5, -2]
+console.log(cogRpm([100, 100, 100, 100], 0)) // [1, -1]
+console.log(cogRpm([77, 54, 47, 51, 81, 79, 62, 40, 48, 28, 66, 70, 76], 2)) // [0.6103896103896104, 1.6382978723404256]
+console.log(cogRpm([80, 38, 87, 28, 71, 58, 94, 74, 98, 68, 19, 54, 67, 97, 87], 1)) // [-0.475, -2.1052631578947367]
+console.log(cogRpm([65, 14, 100, 53, 73, 20, 73,  39, 74, 14], 2)) // [1.5384615384615385, -0.6499999999999999]
+console.log(cogRpm([77, 54, 18, 29, 94, 60, 90, 92, 32, 92, 42], 1)) // [-0.7777777777777777, -1.2857142857142858]
+console.log(cogRpm([20, 69], 0)) // [3.4499999999999997, -0.2898550724637681]
