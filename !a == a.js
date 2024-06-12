28ef25189edf6d317765dc3b912ff66b854e39bb
Steven <majileaf@gmail.com>
2024-06-12 17:11:32 +0200

    Js language check

---
+++ !a == a.js
@@ -0,0 +1,13 @@
+/* !a == a ?!
+You task to pass only this tests :
+
+a == false
+!a == false
+a == !a
+*/
+
+const a = 
+
+console.log(a == false);
+console.log(!a == false);
+console.log(a == !a  );
