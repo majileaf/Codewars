0c38e2952df4a5a5cb0e90f3970d9cc6b331d190
Steven <majileaf@gmail.com>
2024-06-24 21:10:10 +0200

    date comparison using date object

---
+++ 6th Kyu Problems/Count the days!.js
@@ -0,0 +1,19 @@
+/* Count the days!
+Little Annie is very excited for upcoming events. She wants to know how many days she has to wait for a specific event.
+
+Your job is to help her out.
+
+Task: Write a function which returns the number of days from today till the given date. 
+The function will take a Date object as parameter. You have to round the amount of days.
+
+If the event is in the past, return "The day is in the past!"
+If the event is today, return "Today is the day!"
+Else, return "x days"
+*/
+
+function countDays(d){
+  //have fun with coding! :)
+}
+
+console.log(countDays(new Date("February 28, 2016"))) // "The day is in the past!"
+console.log(countDays(new Date())) // "Today is the day!"
