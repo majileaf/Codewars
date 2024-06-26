16cc9382e503d46f5e48e6b18678644edb3b7f7d
Steven <majileaf@gmail.com>
2024-06-27 01:35:51 +0200

    String & Number conditional check

---
+++ 7th Kyu Problems/Most Likely.js
@@ -0,0 +1,19 @@
+/* Most Likely
+Create a function which compares two probabilities, returning true if the first one is most likely otherwise false.
+For this exercise probability is expressed as two numbers separated by a colon e.g. a probability of 1 in 3 will be 1:3.
+
+So:
+Input: ('1:3','1:2') - returns false as 1 in 3 is less likely than 1 in 2.
+*/
+
+const mostLikely = (prob1, prob2) => {
+  const [p1x, p1y] = prob1.split(':');
+  const [p2x, p2y] = prob2.split(':');
+  return p1x / p1y > p2x / p2y;
+}
+
+// alternatively:
+// const getN = str => str.split(':').reduce((sum, n) => sum / n);
+// const mostLikely = (prob1, prob2) => getN(prob1) > getN(prob2);
+
+console.log(mostLikely('1:3','1:2')) // false
