ac6911afa8a27cd413bf974295a1aa17ce7028c5
Steven <majileaf@gmail.com>
2024-07-05 02:11:18 +0200

    Number conditional check

---
+++ 7th Kyu Problems/A === B.js
@@ -0,0 +1,24 @@
+/* A === B
+Compare 2 integers and return if they are equals, don't use the comparison Operators
+
+Can't use these Operators
+
+Operator	Description
+==	equal to
+===	equal value and equal type
+!=	not equal
+!==	not equal value or not equal type
+>	greater than
+<	less than
+>=	greater than or equal to
+<=	less than or equal to
+
+Example:
+  0 == 0  => true 
+  0 == 1  => false 
+*/
+
+const d01 = (a, b) => Object.is(a, b);
+
+console.log(d01(1, 1)) // true
+console.log(d01(2, 1)) // false
