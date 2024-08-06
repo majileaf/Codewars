a0e442c6864bfbf21ae7e2209dc580b9d11cee06
Steven <majileaf@gmail.com>
2024-08-06 14:03:58 +0200

    Object conditional check

---
+++ 7th Kyu Problems/Find an employees role in the company.js
@@ -0,0 +1,54 @@
+/* Find an employees role in the company
+You get a new job working for Eggman Movers. Your first task is to write a method that will allow the admin staff to 
+enter a person’s name and return what that person's role is in the company.
+
+You will be given an array of object literals holding the current employees of the company. You code must find 
+the employee with the matching firstName and lastName and then return the role for that employee or if no employee 
+is not found it should return "Does not work here!"
+
+The array is preloaded and can be referenced using the variable employees ($employees in Ruby). It uses the following structure.
+
+let employees = [ {firstName: "Dipper", lastName: "Pines", role: "Boss"}, ...... ]
+There are no duplicate names in the array and the name passed in will be a single string with a space between the first and 
+last name i.e. Jane Doe or just a name.
+*/
+
+const employees = [
+  { firstName: 'Ollie', lastName: 'Hepburn', role: 'Boss' },
+  { firstName: 'Morty', lastName: 'Smith', role: 'Truck Driver' },
+  { firstName: 'Peter', lastName: 'Ross', role: 'Warehouse Manager' },
+  { firstName: 'Cal', lastName: 'Neil', role: 'Sales Assistant' },
+  { firstName: 'Jesse', lastName: 'Saunders', role: 'Admin' },
+  { firstName: 'Anna', lastName: 'Jones', role: 'Sales Assistant' },
+  { firstName: 'Carmel', lastName: 'Hamm', role: 'Admin' },
+  { firstName: 'Tori', lastName: 'Sparks', role: 'Sales Manager' },
+  { firstName: 'Peter', lastName: 'Jones', role: 'Warehouse Picker' },
+  { firstName: 'Mort', lastName: 'Smith', role: 'Warehouse Picker' },
+  { firstName: 'Anna', lastName: 'Bell', role: 'Admin' },
+  { firstName: 'Jewel', lastName: 'Bell', role: 'Receptionist' },
+  { firstName: 'Colin', lastName: 'Brown', role: 'Trainee' }
+]
+
+const findEmployeesRole = name => {
+  const [first, last] = name.split(' ');
+  const found = employees.find(e => e.firstName === first && e.lastName === last);
+  return found ? found.role : 'Does not work here!';
+}
+
+// alternatively #1:
+// const findEmployeesRole = name => {
+//   const found = employees.find(e => `${e.firstName} ${e.lastName}` === name);
+//   return found ? found.role : 'Does not work here!';
+// }
+
+// alternatively #2:
+// const findEmployeesRole = name => {
+//   for (const employee of employees) {
+//       if (`${employee.firstName} ${employee.lastName}` === name) return employee.role;
+//   }
+//   return 'Does not work here!';
+// }
+
+console.log(findEmployeesRole("Dipper Pines")) // "Does not work here!"
+console.log(findEmployeesRole("Morty Smith")) // "Truck Driver"
+console.log(findEmployeesRole("Anna Bell")) // "Admin"
