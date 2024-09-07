cae853754d9205f1c6ecb02cc5f381dd6ca44343
Steven <majileaf@gmail.com>
2024-09-07 16:20:41 +0200

    String conditional check

---
+++ 7th Kyu Problems/reverseIt.js
@@ -0,0 +1,23 @@
+/* reverseIt
+You have to create a function named reverseIt.
+
+Write your function so that in the case a string or a number is passed in as the data , you will return the data in reverse order. If the data is any other type, return it as it is.
+
+Examples of inputs and subsequent outputs:
+
+"Hello" -> "olleH"
+
+"314159" -> "951413"
+
+[1,2,3] -> [1,2,3]
+*/
+
+const reverseIt = data => 
+    typeof data === 'string' ?  [...data].reverse().join('') :
+    typeof data === 'number' ? +[...String(data)].reverse().join('') :
+    data;
+
+console.log(reverseIt('Hello')) // "olleH"
+console.log(reverseIt(314159)) // 951413
+console.log(reverseIt("314159")) // "951413"
+console.log(reverseIt([])) // []
