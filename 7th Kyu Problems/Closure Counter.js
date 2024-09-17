ba5885cb0cb1ef30c4f68e8c68fd83e4bfecda7c
Steven <majileaf@gmail.com>
2024-09-17 09:12:09 +0200

    Object conditional check + currying

---
+++ 7th Kyu Problems/Closure Counter.js
@@ -0,0 +1,34 @@
+/* Closure Counter
+Define the function counter that returns a function that returns an increasing value.
+The first value should be 1.
+You're going to have to use closures.
+
+Example:
+const newCounter = counter();
+newCounter() // 1
+newCounter() // 2
+
+Closure:
+A closure is the combination of a function bundled together (enclosed) with references to 
+its surrounding state (the lexical environment). 
+
+In other words, a closure gives you access to an outer function’s scope from an inner function. 
+In JavaScript, closures are created every time a function is created, at function creation time.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+*/
+
+function counter() {
+  let i = 1;
+  function add() {
+    return i++;
+  }
+  return add;
+}
+
+// alternatively:
+// const counter = (i = 1) => () => i++;
+
+const newCount = counter();
+console.log(newCount()); // 1
+console.log(newCount()); // 2
