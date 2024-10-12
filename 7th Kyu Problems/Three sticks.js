7dd944e57e145271b111a6b70401fec9da79b0ee
Steven <majileaf@gmail.com>
2024-10-12 14:52:54 +0200

    Number conditional check

---
+++ 7th Kyu Problems/Three sticks.js
@@ -0,0 +1,16 @@
+/* Three sticks
+Imagine that you are given two sticks. You want to end up with three sticks of equal length. 
+You are allowed to cut either or both of the sticks to accomplish this, and can throw away leftover pieces.
+
+Write a function, maxlen, that takes the lengths of the two sticks (L1 and L2, both positive values), 
+that will return the maximum length you can make the three sticks.
+*/
+
+function maxlen(l1, l2) {
+  // return maximum length
+  return 0;
+}
+
+console.log(maxlen(5, 12)) // 5
+console.log(maxlen(12, 5)) // 5
+console.log(maxlen(5, 17)) // 5.666666666666667
