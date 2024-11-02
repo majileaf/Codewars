035c30fec2ec41273ba3e72cceab42f46d9175f9
Steven <majileaf@gmail.com>
2024-11-02 05:37:02 +0100

    Object conditional check

---
+++ 7th Kyu Problems/Grasshopper - Terminal Game #1.js
@@ -0,0 +1,30 @@
+/* Grasshopper - Terminal Game #1
+Terminal Game - Create Hero Prototype
+In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. 
+
+The hero should have the following attributes:
+attribute	value
+name	user argument or 'Hero'
+position	'00'
+health	100
+damage	5
+experience	0
+*/
+
+class Hero {
+  constructor(name = 'Hero') {
+      this.name = name;
+      this.position = '00';
+      this.health = 100;
+      this.damage = 5;
+      this.experience = 0;
+  }
+}
+
+let myHero = new Hero();
+console.log(myHero.name) // 'Hero'
+console.log(myHero.experience) // 0
+console.log(myHero.health) // 100
+console.log(myHero.position) // '00'
+console.log(myHero.damage) // 5
+console.log(new Hero('Greg').name) // 'Greg'
