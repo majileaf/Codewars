9b8a78a2d2486110c1594d985479e9687522b6ce
maj <majileaf@gmail.com>
2025-02-18 14:12:34 +0100

    String & Array conditional check

---
+++ 7th Kyu Problems/Club Doorman.js
@@ -0,0 +1,26 @@
+/* Club Doorman
+INTRODUCTION
+
+The Club Doorman will give you a word. To gain access, you need to provide the right number according to the given word.
+Each word contains exactly one pair of consecutive identical letters, such as the tt in lettuce.
+To determine the number, find the position of the repeated letter in the alphabet (where a is 1, b is 2, ..., z is 26) and 
+multiply it by 3.
+
+TASK
+Implement a function that takes a word as input and returns the correct number.
+
+EXAMPLE
+For the word lettuce, the repeated letter is t, which is the 20th letter in the alphabet.
+The correct answer is 20 × 3 = 60.
+
+NOTE
+You may assume that the given string will
+    always contain exactly one pair of consecutive identical letters.
+    consist only of lowercase English letters.
+*/
+
+const passTheDoorMan = word => ([...word].find((letter, i) => letter === word[i + 1]).charCodeAt() - 96) * 3;
+
+console.log(passTheDoorMan("lettuce")) // 60
+console.log(passTheDoorMan("hill")) // 36
+console.log(passTheDoorMan("apple")) // 48
