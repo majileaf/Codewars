f4dd06d6014c47cfeec1a4793747350247943930
maj <majileaf@gmail.com>
2025-03-10 17:41:54 +0100

    String & Array conditional check

---
+++ 5th Kyu Problems/Find the unique string.js
@@ -0,0 +1,21 @@
+/* Find the unique string
+There is an array of strings. All strings contains similar letters except one. Try to find it!
+
+findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) === 'BbBb'
+findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) === 'foo'
+
+Strings may contain spaces. Spaces are not significant, only non-spaces symbols matters. 
+E.g. string that contains only spaces is like empty string.
+
+It’s guaranteed that array contains more than 2 strings.
+*/
+
+function findUniq(arr) {
+  // do magic
+}
+
+console.log(findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ])) // 'BbBb'
+console.log(findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ])) // 'foo'
+console.log(findUniq([ 'silvia', 'vasili', 'victor' ])) // 'victor'
+console.log(findUniq([ 'Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter' ])) // 'Harry Potter'
+console.log(findUniq([ '    ', 'a', ' ' ])) // 'a'
