9660fc81435570444afe6d541dab2dff0cdd1cfe
maj <majileaf@gmail.com>
2025-04-04 23:25:59 +0200

    Array conditional check

---
+++ 5th Kyu Problems/Fastest Code AxB eqals C.js
@@ -0,0 +1,32 @@
+/* Fastest Code : A*B=C
+This is the Performance version of coding 3min series. If your code runs more than 6000ms, 
+please optimize your code or try the simple version
+
+Task
+You are given an integer array numbers and an integer c.
+
+Find out a pair of numbers(let's say number a and number b) from the array numbers, let a * b = c, 
+result format is an array [a,b]
+
+The array numbers is a sorted array, value range: -1000 to 1000(both inclusive)
+
+The result will be the first pair of numbers(from the left to the right). 
+For example,findAB([1,2,3,4,5,6],6) should return [1,6], instead of [2,3]
+*/
+
+function findAB(numbers,c){
+  //coding and coding...
+}
+
+console.log(findAB([1,2,3], 3)) // [1,3]
+console.log(findAB([1,2,3], 6)) // [2,3]
+console.log(findAB([1,2,3], 7)) // null
+console.log(findAB([1,2,3,6], 6)) // [1,6]
+console.log(findAB([1,2,3,4,5,6], 15)) // [3,5]
+console.log(findAB([0,0,2], 4)) // null
+console.log(findAB([0,0,2,2], 4)) // [2,2]
+console.log(findAB([-3,-2,-2,-1,0,1,2,3,4], 4)) // [-2,-2]
+console.log(findAB([-3,-2,-2,-1,0,1,2,3,4], 0)) // [-3,0]
+console.log(findAB([-3,-2,-1,0,1,2,3,4], 4)) // [1,4]
+console.log(findAB([0,1,2,3], 0)) // [0,1]
+console.log(findAB([0,0,2,3], 0)) // [0,0]
