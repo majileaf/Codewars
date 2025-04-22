edb7c047bf6c9d7fb89b531c79737a5eca44ac10
maj <majileaf@gmail.com>
2025-04-22 19:13:36 +0200

    small fix

--- 6th Kyu Problems/Binary to Text ASCII Conversion.js
+++ 6th Kyu Problems/Binary to Text ASCII Conversion.js
@@ -7,7 +7,7 @@ Characters can be in the range from "00000000" to "11111111" (inclusive)
 Note: In the case of an empty binary string your function should return an empty string.
 */
 
-const binaryToString = binary => binary ? String.fromCharCode(...binary.match(/.{1,8}/g).map(e => parseInt(e, 2))) : '';
+const binaryToString = binary => binary ? String.fromCharCode(...binary.match(/.{8}/g).map(e => parseInt(e, 2))) : '';
 
 console.log(binaryToString('01100001')) // 'a'
 console.log(binaryToString('01001011010101000100100001011000010000100101100101000101')) // 'KTHXBYE'
