687d1a15bf6fc87e3613b5335bf6bce6dfff9a99
maj <majileaf@gmail.com>
2025-06-25 18:35:30 +0200

    Puzzle

--- 7th Kyu Problems/Sentences with Functions.js
+++ 7th Kyu Problems/Sentences with Functions.js
@@ -5,16 +5,17 @@ Adam(has(a(dog()))); // must return "Adam has a dog."
 The(name(of(the(dog(is(also(Adam()))))))); // must return "The name of the dog is also Adam."
 */
 
-function Adam() {}
-function has() {}
-function a() {}
-function dog() {}
-function The() {}
-function name() {}
-function of() {}
-function the() {}
-function is() {}
-function also() {}
+function fn(a) {return a ? ' ' + a : '.'}
+function Adam(a) {return 'Adam' + fn(a)}
+function has(a) {return 'has' + fn(a)}
+function a(a) {return 'a' + fn(a)}
+function dog(a) {return 'dog' + fn(a)}
+function The(a) {return 'The' + fn(a)}
+function name(a) {return 'name' + fn(a)}
+function of(a) {return 'of' + fn(a)}
+function the(a) {return 'the' + fn(a)}
+function is(a) {return 'is' + fn(a)}
+function also(a) {return 'also' + fn(a)}
 
 console.log(Adam(has(a(dog())))) // "Adam has a dog."
 console.log(The(name(of(the(dog(is(also(Adam())))))))) // "The name of the dog is also Adam."
