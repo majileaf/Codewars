435a4873bb0da8efc25dabd7f2c5f316c3f8e42b
maj <majileaf@gmail.com>
2025-07-16 11:46:40 +0200

    Code golfing

---
+++ 7th Kyu Problems/JS Golfing #1 undefined.js
@@ -0,0 +1,10 @@
+/* JS Golfing #1: undefined
+Task:
+Return undefined with no more than 10 characters.
+    Digits and letters are not allowed!
+    Your function should take exactly `0` parameters.
+*/
+
+x=()=>{}
+
+console.log(x()) // undefined
