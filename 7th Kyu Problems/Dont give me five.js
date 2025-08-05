6e7656a1da24c33401d727d00080d983f0af1d7b
maj <majileaf@gmail.com>
2025-08-05 18:09:34 +0200

    Numbers conditional check

---
+++ 7th Kyu Problems/Dont give me five.js
@@ -0,0 +1,33 @@
+/* Don't give me five!
+In this kata you get the start number and the end number of a region 
+and should return the count of all numbers except numbers with a 5 in it. 
+The start and the end number are both inclusive!
+
+Examples:
+1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+
+The result may contain fives.
+The start number will always be smaller than the end number. 
+Both numbers can be also negative!
+*/
+
+const dontGiveMeFive = (start, end) => {
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        if (!String(i).includes('5')) sum++;
+    }
+    return sum;
+}
+
+console.log(dontGiveMeFive(2,5)) // 3
+console.log(dontGiveMeFive(-90,1)) // 74
+console.log(dontGiveMeFive(1,90)) // 72
+console.log(dontGiveMeFive(52,61)) // 2
+console.log(dontGiveMeFive(-61,-52)) // 2
+console.log(dontGiveMeFive(5,15)) // 9
+console.log(dontGiveMeFive(-15,5)) // 18
+console.log(dontGiveMeFive(1,9)) // 8
+console.log(dontGiveMeFive(-9,1)) // 10
+console.log(dontGiveMeFive(4,17)) // 12
+console.log(dontGiveMeFive(-17,4)) // 20
