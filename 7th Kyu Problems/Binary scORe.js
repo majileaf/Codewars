fdac1416f782d9a39af81948abf208ad0b1abaab
maj <majileaf@gmail.com>
2025-08-25 00:24:41 +0200

    Math

---
+++ 7th Kyu Problems/Binary scORe.js
@@ -0,0 +1,19 @@
+/* Binary scORe
+Given a number n, define its scORe to be 0 | 1 | 2 | 3 | .. | n, where | is the bitwise OR operator.
+
+Write a function that takes n and finds its scORe.
+
+        n |   scORe n
+----------|----------
+        0 |         0
+        1 |         1
+       49 |        63
+1 000 000 | 1 048 575
+*/
+
+const score = n => Math.max(2 ** Math.floor(Math.log(n) / Math.log(2)) * 2 - 1, 0);
+
+console.log(score(0)) // 0
+console.log(score(1)) // 1
+console.log(score(49)) // 63
+console.log(score(1000000)) // 1048575
