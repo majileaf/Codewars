220c50fa0095db7c21646a6e99fd2b9cd2341f5d
maj <majileaf@gmail.com>
2025-10-20 19:02:15 +0200

    String regex

---
+++ 6th Kyu Problems/String subpattern recognition I.js
@@ -0,0 +1,27 @@
+/* String subpattern recognition IString subpattern recognition I
+In this kata you need to build a function to return either true/True or false/False if a string can be seen as 
+the repetition of a simpler/shorter subpattern or not.
+
+For example:
+hasSubpattern("a") === false; //no repeated pattern
+hasSubpattern("aaaa") === true; //created repeating "a"
+hasSubpattern("abcd") === false; //no repeated pattern
+hasSubpattern("abababab") === true; //created repeating "ab"
+hasSubpattern("ababababa") === false; //cannot be entirely reproduced repeating a pattern
+
+Strings will never be empty and can be composed of any character (just consider upper- and lowercase letters as 
+different entities) and can be pretty long (keep an eye on performances!).
+*/
+
+const hasSubpattern = string => /^(\w+)\1+$/g.test(string);
+
+console.log(hasSubpattern("a")) //            false
+console.log(hasSubpattern("aaaa")) //         true
+console.log(hasSubpattern("abcd")) //         false
+console.log(hasSubpattern("abababab")) //     true
+console.log(hasSubpattern("ababababa")) //    false
+console.log(hasSubpattern("123a123a123a")) // true
+console.log(hasSubpattern("123A123a123a")) // false
+console.log(hasSubpattern("abbaabbaabba")) // true
+console.log(hasSubpattern("abbabbabba")) //   false
+console.log(hasSubpattern("abcdabcabcd")) //  false
