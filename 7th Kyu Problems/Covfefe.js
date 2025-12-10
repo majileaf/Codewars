36a8d71af32b18851922184774484f52c4afbd6a
maj <majileaf@gmail.com>
2025-12-10 17:51:32 +0100

    String

---
+++ 7th Kyu Problems/Covfefe.js
@@ -0,0 +1,20 @@
+/* Covfefe
+
+Your are given a string. You must replace any occurence of the sequence coverage by covfefe, 
+however, if you don't find the word coverage in the string, you must add covfefe at the end 
+of the string with a leading space.
+
+For the languages where the string is mutable (such as ruby), don't modify the given string, 
+otherwise this will break the test cases.
+*/
+
+const covfefe = str => str.includes('coverage') 
+    ? str.replaceAll('coverage', 'covfefe') 
+    : str + ' covfefe';
+
+console.log(covfefe("coverage")) // "covfefe"
+console.log(covfefe("coverage coverage")) // "covfefe covfefe"
+console.log(covfefe("nothing")) // "nothing covfefe"
+console.log(covfefe("double space ")) // "double space  covfefe"
+console.log(covfefe("covfefe")) // "covfefe covfefe"
+console.log(covfefe("erag")) // "erag covfefe"
