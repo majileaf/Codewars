69cb1bbca4353cf970a17a9242f0566f54dd9cd0
maj <majileaf@gmail.com>
2025-12-18 07:04:44 +0100

    String & Array

---
+++ 7th Kyu Problems/Palindrome Pairs.js
@@ -0,0 +1,31 @@
+/* Palindrome Pairs
+Given a list of unique words. Find all pairs of distinct indices (i, j) in the given list so that the concatenation of 
+the two words, i.e. words[i] + words[j] is a palindrome.
+
+Examples:
+
+["bat", "tab", "cat"] # [[0, 1], [1, 0]]
+["dog", "cow", "tap", "god", "pat"] # [[0, 3], [2, 4], [3, 0], [4, 2]]
+["abcd", "dcba", "lls", "s", "sssll"] # [[0, 1], [1, 0], [2, 4], [3, 2]]
+
+Non-string inputs should be converted to strings.
+
+Return an array of arrays containing pairs of distinct indices that form palindromes. Pairs should be returned in the order 
+they appear in the original list.
+*/
+
+const checkPal = (word1, word2) => word1 + word2 === [...word1 + word2].reverse().join('');
+const palindromePairs = words => {
+    words = words.map(String);
+    const res = [];
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words.length; j++) {
+            if (i !== j && checkPal(words[i], words[j])) res.push([i, j]);
+        }
+    }
+    return res;
+}
+
+console.log(palindromePairs(["bat", "tab", "cat"])) // [[0, 1], [1, 0]]
+console.log(palindromePairs(["dog", "cow", "tap", "god", "pat"])) // [[0, 3], [2, 4], [3, 0], [4, 2]
+console.log(palindromePairs(["abcd", "dcba", "lls", "s", "sssll"])) // [[0, 1], [1, 0], [2, 4], [3, 2]
