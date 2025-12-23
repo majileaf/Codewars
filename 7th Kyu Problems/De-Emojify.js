20be9c6f7145ef10a1a7532ee6aa9e0dd42e72b8
maj <majileaf@gmail.com>
2025-12-23 17:01:51 +0100

    String

---
+++ 7th Kyu Problems/De-Emojify.js
@@ -0,0 +1,36 @@
+/* De-Emojify
+Inspired by the emojify custom Python module.
+
+You are given a string made up of chains of emotes separated by 1 space each, with chains having 2 spaces in-between each.
+
+Each emote represents a digit:
+
+:)  | 0
+:D  | 1
+>(  | 2
+>:C | 3
+:/  | 4
+:|  | 5
+:O  | 6
+;)  | 7
+^.^ | 8
+:(  | 9
+
+Each emote chain represents the digits of the ASCII/Unicode code for a character, e.g. :( ;) is 97, 
+which is the ASCII code for 'a'.
+
+Given a such string of emotes, find the string it represents. Example:
+
+':D :) :/  :D :) :|' is 2 chains: ':D :) :/' and ':D :) :|'.
+
+These represent ASCII codes 104 and 105 respectively, translating to 'hi'.
+
+Input will always be valid. Chains may start with leading zeroes; these are valid and do not change the chain's value.
+*/
+
+const emoMap = {':)': 0, ':D': 1, '>(': 2, '>:C':3, ':/': 4, ':|': 5, ':O': 6, ';)': 7, '^.^':8, ':(': 9};
+const deEmojify = emojiString => emojiString.length ? String.fromCharCode(...emojiString.split('  ').map(e => e.split(' ').map(f => emoMap[f]).join(''))) : '';
+
+console.log(deEmojify(":D :) :/  :D :) :|")) // "hi"
+console.log(deEmojify(";) >(  :D :) :D  :D :) ^.^  :D :) ^.^  :D :D :D  >:C >(  :D :D :(  :D :D :D  :D :D :/  :D :) ^.^  :D :) :)  >:C >:C")) // "Hello world!"
+console.log(deEmojify(":)")) // "\x00"
