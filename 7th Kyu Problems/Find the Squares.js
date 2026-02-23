742861fef6f14d00a9426192df3492be2f1215ea
maj <majileaf@gmail.com>
2026-02-23 17:46:21 +0100

    String

---
+++ 7th Kyu Problems/Find the Squares.js
@@ -0,0 +1,23 @@
+/* Find the Squares
+Problem
+Complete the function that takes an odd integer (0 < n < 1000000) which is the difference between two consecutive perfect squares, 
+and return these squares as a string in the format "bigger-smaller"
+
+Examples
+9  -->  "25-16"
+5  -->  "9-4"
+7  -->  "16-9"
+*/
+
+const findSquares = num => {
+    let i = 0;
+    while (true) {
+        if ((i + 1) ** 2 - i ** 2 === num) return `${(i + 1) ** 2}-${i ** 2}`;
+        i++;
+    }
+};
+
+console.log(findSquares(9)) // '25-16'
+console.log(findSquares(5)) // '9-4'
+console.log(findSquares(81)) // '1681-1600'
+console.log(findSquares(25)) // '169-144'
