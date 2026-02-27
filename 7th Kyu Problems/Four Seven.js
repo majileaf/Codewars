7f38d64170e4d5628d028ee07afb62ebdf5ae6f3
maj <majileaf@gmail.com>
2026-02-27 20:16:33 +0100

    Puzzle

---
+++ 7th Kyu Problems/Four Seven.js
@@ -0,0 +1,16 @@
+/* Four Seven
+Simple kata, simple rules: your function should accept the inputs 4 and 7. If 4 is entered, the function should return 7. 
+If 7 is entered, the function should return 4. Anything else entered as input should return 0. 
+There's only one catch, your function cannot include if statements, switch statements, or the ternary operator 
+(or the eval function due to the fact that you can get around the if requirement using it).
+
+There are some very simple ways of answering this problem, but I encourage you to try and be as creative as possible.
+
+Good Luck!
+*/
+
+const fourSeven = n => ({4:7, 7:4})[n] || 0;
+
+console.log(fourSeven(4)) // 7
+console.log(fourSeven(7)) // 4
+console.log(fourSeven(1)) // 0
