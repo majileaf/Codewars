abab9a5ac8564861e810fd8c4b5bd508abfc0606
maj <majileaf@gmail.com>
2026-03-14 17:26:18 +0100

    String regex

---
+++ 6th Kyu Problems/Adjacent repeated words in a string.js
@@ -0,0 +1,41 @@
+/* Adjacent repeated words in a string
+You know how sometimes you write the the same word twice in a sentence, but then don't notice that it happened? 
+For example, you've been distracted for a second. Did you notice that "the" is doubled in the first sentence of this description?
+
+As as aS you can see, it's not easy to spot those errors, especially if words differ in case, like "as" at the beginning of 
+this sentence.
+
+Write a function that counts the number of sections repeating the same word (case insensitive). 
+The occurence of two or more equal words next after each other counts as one.
+
+Examples
+"dog cat"                  -->  0
+"dog DOG cat"              -->  1
+"apple dog cat"            -->  0
+"pineapple apple dog cat"  -->  0
+"apple apple dog cat"      -->  1
+"apple dog apple dog cat"  -->  0
+"dog dog DOG dog dog dog"  -->  1
+"dog dog dog dog cat cat"  -->  2
+"cat cat dog dog cat cat"  -->  3
+*/
+
+function countAdjacentPairs(searchString) {
+  
+}
+
+console.log(countAdjacentPairs("dog cat"                )) // 0
+console.log(countAdjacentPairs("dog DOG cat"            )) // 1
+console.log(countAdjacentPairs("apple dog cat"          )) // 0
+console.log(countAdjacentPairs("pineapple apple dog cat")) // 0
+console.log(countAdjacentPairs("apple apple dog cat"    )) // 1
+console.log(countAdjacentPairs("apple dog apple dog cat")) // 0
+console.log(countAdjacentPairs("dog dog DOG dog dog dog")) // 1
+console.log(countAdjacentPairs("dog dog dog dog cat cat")) // 2
+console.log(countAdjacentPairs("cat cat dog dog cat cat")) // 3
+console.log(countAdjacentPairs('')) // 0, 'An empty string should return 0.'
+console.log(countAdjacentPairs('orange Orange kiwi pineapple apple')) // 1, "Case should be ignored."
+console.log(countAdjacentPairs('banana banana banana')) // 1, "Once a word has been paired, it is ignored."
+console.log(countAdjacentPairs('banana banana banana terracotta banana terracotta terracotta pie!')) // 2, "Once a word has been paired, it is ignored. Grab all pairs."
+console.log(countAdjacentPairs('pineapple apple')) // 0, "A pineapple is not an apple."
+console.log(countAdjacentPairs("pineapple apple apple apple apple apple applepine")) // 1
