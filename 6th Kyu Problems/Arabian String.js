9a5c42d2b06e459a2fbacf5b62a5698b431fd7b6
maj <majileaf@gmail.com>
2026-03-19 17:44:53 +0100

    String regex

---
+++ 6th Kyu Problems/Arabian String.js
@@ -0,0 +1,16 @@
+/* Arabian String
+You must create a method that can convert a string from any format into PascalCase. This must support symbols too.
+
+Don't presume the separators too much or you could be surprised.
+
+For example: (Input --> Output)
+
+"example name" --> "ExampleName"
+"your-NaMe-here" --> "YourNameHere"
+"testing ABC" --> "TestingAbc"
+*/
+
+const camelize = str => (str.match(/[a-z0-9]+/gi) || []).map(e => e[0].toUpperCase() + e.slice(1).toLowerCase()).join('');
+
+console.log(camelize("java script")) // "JavaScript"
+console.log(camelize("cODE warS")) // "CodeWars"
