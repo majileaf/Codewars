37de6d3f96951b5388d57e35dfc031e803b083ff
maj <majileaf@gmail.com>
2026-04-02 20:14:58 +0200

    String

---
+++ 6th Kyu Problems/Find Added .js
@@ -0,0 +1,24 @@
+/* Find Added 
+You are given two strings (st1, st2) as inputs. Your task is to return a string containing the numbers in st2 
+which are not in str1. Make sure the numbers are returned in ascending order. All inputs will be a string of numbers.
+
+Here are some examples:
+findAdded('4455446', '447555446666'); // '56667'
+findAdded('44554466', '447554466'); // '7'
+findAdded('9876521', '9876543211'); // '134'
+findAdded('678', '876'); // ''
+findAdded('678', '6'); // ''
+*/
+
+const findAdded = (st1, st2) => {
+    for (let i = 0; i < st1.length; i++) {
+        st2  = st2.replace(st1[i] , '');
+    }
+    return st2.split('').sort((a, b) => a - b).join('');
+}
+
+console.log(findAdded('44554466', '447554466')) // '7'
+console.log(findAdded('9876521', '9876543211')) // '134'
+console.log(findAdded('4455446', '447555446666')) // '56667'
+console.log(findAdded('678', '876')) // ''
+console.log(findAdded('678', '6')) // ''
