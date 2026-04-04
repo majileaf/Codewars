dd6cce04e8d250ae622b183ec85c0aef601038f0
maj <majileaf@gmail.com>
2026-04-04 16:55:50 +0200

    String

---
+++ 6th Kyu Problems/Fat Fingers.js
@@ -0,0 +1,43 @@
+/* Fat Fingers
+Freddy has a really fat left pinky finger, and every time Freddy tries to type an A, he accidentally hits the CapsLock key!
+
+Given a string that Freddy wants to type, emulate the keyboard misses where each A supposedly pressed is replaced with CapsLock, 
+and return the string that Freddy actually types. It doesn't matter if the A in the string is capitalized or not. 
+When CapsLock is enabled, capitalization is reversed, but punctuation is not affected.
+
+Examples:
+"The quick brown fox jumps over the lazy dog."
+-> "The quick brown fox jumps over the lZY DOG."
+
+"The end of the institution, maintenance, and administration of government, is to secure the existence of the body politic, to protect it, and to furnish the individuals who compose it with the power of enjoying in safety and tranquillity their natural rights, and the blessings of life: and whenever these great objects are not obtained, the people have a right to alter the government, and to take measures necessary for their safety, prosperity and happiness."
+-> "The end of the institution, mINTENnce, ND dministrTION OF GOVERNMENT, IS TO SECURE THE EXISTENCE OF THE BODY POLITIC, TO PROTECT IT, nd to furnish the individuLS WHO COMPOSE IT WITH THE POWER OF ENJOYING IN Sfety ND TRnquillity their nTURl rights, ND THE BLESSINGS OF LIFE: nd whenever these greT OBJECTS re not obtINED, THE PEOPLE Hve  RIGHT TO lter the government, ND TO Tke meSURES NECESSry for their sFETY, PROSPERITY nd hPPINESS."
+
+"aAaaaaAaaaAAaAa"
+-> ""
+*/
+
+const fatFingers = str => {
+    if (str === null) return null;
+    str = str.split('');
+    let upCase = false;
+    let string = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (/a/i.test(str[i])) {
+            upCase = upCase ? false : true;
+            str[i] = '';
+        }
+        string += upCase 
+            ? str[i] === str[i].toUpperCase() 
+                ? str[i].toLowerCase() 
+                : str[i].toUpperCase() 
+            : str[i];
+    }
+    return string;
+};
+
+console.log(fatFingers("The quick brown fox jumps over the lazy dog.")) //  "The quick brown fox jumps over the lZY DOG."
+console.log(fatFingers("aAaaaaAaaaAAaAa")) // ""
+console.log(fatFingers("The end of the institution, maintenance, and administration of government, is to secure the existence of the body politic, to protect it, and to furnish the individuals who compose it with the power of enjoying in safety and tranquillity their natural rights, and the blessings of life: and whenever these great objects are not obtained, the people have a right to alter the government, and to take measures necessary for their safety, prosperity and happiness.")) // "The end of the institution, mINTENnce, ND dministrTION OF GOVERNMENT, IS TO SECURE THE EXISTENCE OF THE BODY POLITIC, TO PROTECT IT, nd to furnish the individuLS WHO COMPOSE IT WITH THE POWER OF ENJOYING IN Sfety ND TRnquillity their nTURl rights, ND THE BLESSINGS OF LIFE: nd whenever these greT OBJECTS re not obtINED, THE PEOPLE Hve  RIGHT TO lter the government, ND TO Tke meSURES NECESSry for their sFETY, PROSPERITY nd hPPINESS."
+console.log(fatFingers(null)) // null
+console.log(fatFingers('abABaBabAb')) // 'BBbbB
