663d75bba398efcecac2d33bb3027fbf71912ed0
maj <majileaf@gmail.com>
2026-04-25 20:31:30 +0200

    Array

---
+++ 6th Kyu Problems/Separating Strings.js
@@ -0,0 +1,71 @@
+/* Separating Strings
+Create a function that takes a string and separates it into a sequence of letters.
+
+The array will be formatted as so:
+
+[['J','L','L','M']
+,['u','i','i','a']
+,['s','v','f','n']
+,['t','e','e','']]
+
+The function should separate each word into individual letters, with the first word in the sentence having its letters 
+in the 0th index of each 2nd dimension array, and so on.
+
+Shorter words will have an empty string in the place once the word has already been mapped out. (See the last element 
+in the last part of the array.)
+
+Examples:
+sepStr("Just Live Life Man")
+// => [['J','L','L','M'],
+// => ['u','i','i','a'],
+// => ['s','v','f','n'],
+// => ['t','e','e','']]);
+
+sepStr("The Mitochondria is the powerhouse of the cell")
+// => [ [ 'T', 'M', 'i', 't', 'p', 'o', 't', 'c' ],
+// => [ 'h', 'i', 's', 'h', 'o', 'f', 'h', 'e' ],
+// => [ 'e', 't', '', 'e', 'w', '', 'e', 'l' ],
+// => [ '', 'o', '', '', 'e', '', '', 'l' ],
+// => [ '', 'c', '', '', 'r', '', '', '' ],
+// => [ '', 'h', '', '', 'h', '', '', '' ],
+// => [ '', 'o', '', '', 'o', '', '', '' ],
+// => [ '', 'n', '', '', 'u', '', '', '' ],
+// => [ '', 'd', '', '', 's', '', '', '' ],
+// => [ '', 'r', '', '', 'e', '', '', '' ],
+// => [ '', 'i', '', '', '', '', '', '' ],
+// => [ '', 'a', '', '', '', '', '', '' ]]
+*/
+
+const sepStr = str => {
+    str = str.split(' ');
+    const x = str.length;
+    const y = Math.max(...str.map(e => e.length));
+    const arr = [...Array(y)].map(_ => Array(x).fill(''));
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < str[i].length; j++) {
+            arr[j][i] = str[i][j];
+        }
+    }
+    return arr;
+}
+
+console.log(sepStr("Just Live Life Man"))
+// [['J','L','L','M'],
+// ['u','i','i','a'],
+// ['s','v','f','n'],
+// ['t','e','e','']]
+
+console.log(sepStr("The Mitochondria is the powerhouse of the cell"))
+// [[ 'T', 'M', 'i', 't', 'p', 'o', 't', 'c' ],
+//   [ 'h', 'i', 's', 'h', 'o', 'f', 'h', 'e' ],
+//   [ 'e', 't', '', 'e', 'w', '', 'e', 'l' ],
+//   [ '', 'o', '', '', 'e', '', '', 'l' ],
+//   [ '', 'c', '', '', 'r', '', '', '' ],
+//   [ '', 'h', '', '', 'h', '', '', '' ],
+//   [ '', 'o', '', '', 'o', '', '', '' ],
+//   [ '', 'n', '', '', 'u', '', '', '' ],
+//   [ '', 'd', '', '', 's', '', '', '' ],
+//   [ '', 'r', '', '', 'e', '', '', '' ],
+//   [ '', 'i', '', '', '', '', '', '' ],
+//   [ '', 'a', '', '', '', '', '', '' ]]
