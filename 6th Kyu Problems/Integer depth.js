9f971f9128ab2e4cad408ee01bd484c0dff54f51
maj <majileaf@gmail.com>
2026-04-27 19:24:02 +0200

    Numbers

---
+++ 6th Kyu Problems/Integer depth.js
@@ -0,0 +1,42 @@
+/* Integer depth
+The depth of an integer n is defined to be how many multiples of n it is necessary to compute before all 10 digits 
+have appeared at least once in some multiple.
+
+example:
+
+let see n = 42
+
+Multiple         value         digits     comment
+42*1              42            2,4 
+42*2              84             8         4 existed
+42*3              126           1,6        2 existed
+42*4              168            -         all existed
+42*5              210            0         2,1 existed
+42*6              252            5         2 existed
+42*7              294            9         2,4 existed
+42*8              336            3         6 existed 
+42*9              378            7         3,8 existed
+
+Looking at the above table under digits column you can find all the digits from 0 to 9, 
+Hence it required 9 multiples of 42 to get all the digits. So the depth of 42 is 9. 
+Write a function which computes the depth of its integer argument. 
+Only strictly positive integers will be passed as an input.
+*/
+
+const computeDepth = x => {
+    const arr = [...new Set(String(x))];
+    let i = 1; let mult = 0;
+
+    while (arr.length < 10) {
+        mult = x * i;
+        let xStr = String(mult);
+        for (let j = 0; j < xStr.length; j++) {
+            if (!arr.includes(xStr[j])) arr.push(xStr[j]);
+        }
+        i++;
+    }
+    return i - 1;
+}
+
+console.log(computeDepth(1)) // 10
+console.log(computeDepth(42)) // 9
