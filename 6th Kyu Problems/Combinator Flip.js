fa10c15c295bf5043010207b24838e8b6cb201a8
maj <majileaf@gmail.com>
2026-05-03 19:46:52 +0200

    Currying

---
+++ 6th Kyu Problems/Combinator Flip.js
@@ -0,0 +1,21 @@
+/*
+Create a combinator function named flip that takes a function as an argument and returns that function with it's arguments reversed.
+
+For example:
+flip(print)(4,5) // returns "5 -> 4"
+
+function print(a,b) {
+  return a + " -> " + b;
+}
+
+The idea is to reverse any number of arguments using a higher order function, without any concern for the function 
+being passed into it. 
+*/
+
+const flip = fn => (...args) => fn(...args.reverse());
+
+function print(a,b) {
+  return a + " -> " + b;
+}
+
+console.log(flip(print)(4,5)) // '5 -> 4'
