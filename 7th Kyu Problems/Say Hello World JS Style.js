8bb4bb9932b0683aa6d9cc215920ab0eed1f0973
maj <majileaf@gmail.com>
2026-05-21 16:29:05 +0200

    Curry

---
+++ 7th Kyu Problems/Say Hello World JS Style.js
@@ -0,0 +1,11 @@
+/* Say "Hello World" JS Style
+Write the definition of the function "say" such that calling this:
+
+say("Hello")("World")
+
+returns "Hello World"
+*/
+
+const say = string1 => string2 => string1 + ' ' + string2;
+
+console.log(say('Hello')('World')) // 'Hello World'
