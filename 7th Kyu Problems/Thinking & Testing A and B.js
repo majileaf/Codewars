d46db4976d26374c1c75850d11b6095fbbe5333d
maj <majileaf@gmail.com>
2026-06-07 18:17:08 +0200

    Puzzle

---
+++ 7th Kyu Problems/Thinking & Testing A and B.js
@@ -0,0 +1,24 @@
+/* Thinking & Testing: A and B?
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+*/
+
+function testit(a,b){
+  //return a+b ?
+  //return a*b ?
+  //return a?b;
+}
+
+//a+b?
+console.log(testit(0,1)) // 1
+console.log(testit(1,2)) // 3
+console.log(testit(10,20)) // 30
+//a*b?
+console.log(testit(1,1)) // 1
+console.log(testit(1,3)) // 3
+//try "Attempt" find more...
