91dfbf430f6a8dc94746b9e57417bc7257684fa4
maj <majileaf@gmail.com>
2026-06-26 17:05:08 +0200

    Puzzle

---
+++ 7th Kyu Problems/Get the lost toString back.js
@@ -0,0 +1,25 @@
+/* Get the lost toString back
+There are some functions whose toString methods have been modified.
+
+function foo() {}
+foo.toString = function() {
+  return 'blablabla';
+};
+
+Write a function _originalToString which takes a function as the argument, 
+and returns a string which is as same as the argument's original toString's.
+Note: You should not change the argument.
+
+For example, the following statement should return function foo() {} instead of blablabla:
+_originalToString(foo); // function foo() {}
+*/
+
+const _originalToString = func => Function.prototype.toString.call(func);
+
+function foo() {}
+var original = foo.toString();
+foo.toString = function() {
+  return 'blablabla';
+};
+
+console.log(_originalToString(foo)) // original
