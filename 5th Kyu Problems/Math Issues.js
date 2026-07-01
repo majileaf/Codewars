32d593d0dd1d33576d73b04d9bf98d8d72ea041b
maj <majileaf@gmail.com>
2026-07-01 19:21:10 +0200

    Numbers

---
+++ 5th Kyu Problems/Math Issues.js
@@ -0,0 +1,29 @@
+/* Math Issues
+Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? 
+We can assure, that only non-negative numbers are passed as arguments. 
+So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
+
+Here is a list of functions, we need:
+    Math.round()
+    Math.ceil()
+    Math.floor()
+*/
+
+Math.round = function(number) {
+  return number + 0.5 | 0;
+};
+
+Math.ceil = function(number) {
+  return number % 1 ? ~~number + 1 : number;
+};
+
+Math.floor = function(number) {
+  return ~~number;
+};
+
+console.log(Math.round(0.4)) // 0
+console.log(Math.round(0.5)) // 1
+console.log(Math.ceil(0.4)) // 1
+console.log(Math.ceil(0.5)) // 1
+console.log(Math.floor(0.4)) // 0
+console.log(Math.floor(0.5)) // 0
