8209a9ca2199f972849ef6dcfa0fd16f76d05a26
maj <majileaf@gmail.com>
2026-07-12 11:14:43 +0200

    Numbers

---
+++ 7th Kyu Problems/Figurate Numbers #2 Pronic Number.js
@@ -0,0 +1,40 @@
+/* Figurate Numbers #2 - Pronic Number
+Task:
+You have to create a function to check whether the argument passed is a Pronic Number and return true if it is & false otherwise.
+
+Description:
+Pronic Number -A pronic number, oblong number, rectangular number or heteromecic number, is a number which is the product of 
+two consecutive integers, that is, n(n + 1).
+
+    The first few Pronic Numbers are - 0, 2, 6, 12, 20, 30, 42...
+
+Explanation:
+  0 = 0 × 1   // ∴  0 is a Pronic Number
+  2 = 1 × 2   // ∴  2 is a Pronic Number
+  6 = 2 × 3   // ∴  6 is a Pronic Number
+ 12 = 3 × 4   // ∴ 12 is a Pronic Number
+ 20 = 4 × 5   // ∴ 20 is a Pronic Number
+ 30 = 5 × 6   // ∴ 30 is a Pronic Number
+ 42 = 6 × 7   // ∴ 42 is a Pronic Number
+
+Note:
+Negative numbers are tested as well.
+*/
+
+const isPronic = n => {
+    let i = 0;
+    while (i * (i + 1) < n) {
+        i++;
+    }
+    return n === i * (i + 1);
+}
+
+console.log(isPronic(0)) // true
+console.log(isPronic(1)) // false
+console.log(isPronic(2)) // true
+console.log(isPronic(3)) // false
+console.log(isPronic(4)) // false
+console.log(isPronic(5)) // false
+console.log(isPronic(6)) // true
+console.log(isPronic(-3)) // false
+console.log(isPronic(-27)) // false
