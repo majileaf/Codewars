74ef75f61f0c56b732daae524e5d839d426ad67a
maj <majileaf@gmail.com>
2026-07-13 19:14:30 +0200

    Array

---
+++ 7th Kyu Problems/Train to remove duplicates from an array with filter.js
@@ -0,0 +1,22 @@
+/* Train to remove duplicates from an array with filter()
+In this kata your task is to remove all the duplicates from the array using a standart build-in method 
+- Array.prototype.filter(); return the array containing unique values only.
+
+Tip: use the index of value(s) to solve this kata
+
+If you are not familiar with filter() - info is here:
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+For example:
+var arr = [4];
+unique(arr); // should return [4]
+
+var arr = [1,1,1,2,2,3];
+unique(arr); // should return [1,2,3]
+*/
+
+const unique = arr => arr.filter((e, i) => arr.indexOf(e) === i);
+
+console.log(unique([4])) // [4]
+console.log(unique([1,2,2,3])) // [1,2,3]
