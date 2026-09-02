85829130d943c6cdb8c2583a970bc41349d3adaa
maj <majileaf@gmail.com>
2026-09-02 16:40:57 +0200

    String

---
+++ 7th Kyu Problems/Sentence to words.js
@@ -0,0 +1,10 @@
+/* Sentence to words
+Write function splitSentence which will create a list of strings from a string.
+
+Example:
+"hello world" -> ["hello", "world"]
+*/
+
+const splitSentence = s => s.split(' ');
+
+console.log(splitSentence("hello world")) // ["hello", "world"]
